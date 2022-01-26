bbc15048758cd41df966bfcefb4c4b9d2739cc57
Tahmid Khan <tahmid.hm.dev@gmail.com>
2022-01-27 00:31:50 +0600

    Initial admin site set up

--- backend/backend/root/admin.py
+++ backend/backend/root/admin.py
@@ -1,3 +1,36 @@
+from django.contrib import admin
 from django.contrib.auth import get_user_model
 
+from .models import AirdropGate, AirdropList, Requirement
+
+
 User = get_user_model()
+
+
+# Set up admin site titles
+
+admin.site.site_title = "NFTY Labs Admin"
+admin.site.site_header = "NFTY Labs Admin"
+admin.site.index_title = "NFTY Labs Admin"
+
+
+# Admin registrations
+
+@admin.register(Requirement)
+class RequirementAdmin(admin.ModelAdmin):
+	list_display = ("tokengate", "chain", "asset_type")
+	search_fields = ("tokengate__title", "chain", "asset_type")
+
+
+@admin.register(AirdropGate)
+class AirdropGateAdmin(admin.ModelAdmin):
+	list_display = ("title", "chain", "asset_type", "user")
+	search_fields = ("title", "chain", "asset_type", "user__username")
+
+
+@admin.register(AirdropList)
+class AirdropListAdmin(admin.ModelAdmin):
+	list_display = ("airdropgate", "wallet_address")
+	search_fields = ("airdropgate__title", "wallet_address")
+
+

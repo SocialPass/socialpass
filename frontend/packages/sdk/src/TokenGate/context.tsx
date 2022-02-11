fde003cdfc0414716c3afa561913eeb01de22ede
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-02-10 19:02:27 -0500

    [frontend] pass styles to context.

--- frontend/packages/sdk/src/TokenGate/context.tsx
+++ frontend/packages/sdk/src/TokenGate/context.tsx
@@ -3,11 +3,11 @@ import { TokenGateContextInterface, TokenGateProviderInterface } from './props';
 
 export const TokenGateContext = createContext<TokenGateContextInterface>({} as TokenGateContextInterface);
 
-export const TokenGateProvider = ({ children, id, gateType }:TokenGateProviderInterface) => {
+export const TokenGateProvider = ({ children, id, gateType, styles }:TokenGateProviderInterface) => {
   const [step, setStep] = useState(0);
   const [json, setJson] = useState(0);
   return (
-	<TokenGateContext.Provider value={{step, setStep, json, setJson, id, gateType}}>
+	<TokenGateContext.Provider value={{id, gateType, styles, step, setStep, json, setJson}}>
 	  {children}
 	</TokenGateContext.Provider>
   );

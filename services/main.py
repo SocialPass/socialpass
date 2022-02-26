21fbfd4b239a01f6e68cf46e53a908a2512dcc88
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-02-26 10:48:45 -0500

    [services] add requirements router

--- services/main.py
+++ services/main.py
@@ -1,12 +1,10 @@
 from fastapi import Depends, FastAPI
 from routers.ticketing import routes as TicketingRouter
+from routers.requirements import routes as RequirementsRouter
 
 # init app
 app = FastAPI()
 
 # setup routers
 app.include_router(TicketingRouter.router)
-
-@app.get("/")
-async def root():
-    return {"message": "Hello Bigger Applications!"}
+app.include_router(RequirementsRouter.router)

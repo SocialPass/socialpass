90ff7fca9f733326b7d72a62e930f6b747beb57a
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-02-26 11:08:28 -0500

    [services] gitignore routers/ticketing

--- services/main.py
+++ services/main.py
@@ -1,10 +1,8 @@
 from fastapi import Depends, FastAPI
-from routers.ticketing import routes as TicketingRouter
 from routers.requirements import routes as RequirementsRouter
 
 # init app
 app = FastAPI()
 
 # setup routers
-app.include_router(TicketingRouter.router)
 app.include_router(RequirementsRouter.router)

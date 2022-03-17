f1dc11147c90ca7fc84992b57c1075858f654988
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-03-16 23:30:25 -0400

    [django] set media storage default_acl = public-read

--- django/config/storages.py
+++ django/config/storages.py
@@ -9,5 +9,6 @@ class StaticRootS3Boto3Storage(S3Boto3Storage):
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = "media"
+    default_acl = "public-read"
     file_overwrite = False
     custom_domain = False

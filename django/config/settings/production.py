42ae79322d46042e112c4e84b5869f7e1ec27b16
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-03-16 20:08:46 -0400

    [django] update s3 settings

--- django/config/settings/production.py
+++ django/config/settings/production.py
@@ -54,8 +54,8 @@ AWS_S3_OBJECT_PARAMETERS = {
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
 AWS_S3_REGION_NAME = env("DJANGO_AWS_S3_REGION_NAME", default=None)
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#cloudfront
-AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
-aws_s3_domain = AWS_S3_CUSTOM_DOMAIN # or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_S3_ENDPOINT_URL = env("DJANGO_AWS_S3_ENDPOINT_URL", default=None)
+aws_s3_domain = AWS_S3_ENDPOINT_URL # or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
 # STATIC
 # ------------------------------------------------------------------------------
--- django/config/storages.py
+++ django/config/storages.py
@@ -4,8 +4,10 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticRootS3Boto3Storage(S3Boto3Storage):
     location = "static"
     default_acl = "public-read"
+    custom_domain = False
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
+    custom_domain = False

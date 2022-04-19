68aa50048e0c7df53324581a8aaf5f78e0a44a2d
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-04-19 15:00:06 -0400

    [frontend] cleanup CheckoutWeb3 logic regarding checkoutSelection

--- frontend/packages/tokengate/src/pages/CheckoutWeb3.tsx
+++ frontend/packages/tokengate/src/pages/CheckoutWeb3.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {  useAccount } from 'wagmi'
 import { Web3ConnectWallet, Web3CheckoutSelection, Web3CheckoutConfirmation } from '../components';
+import { TokenGateContext } from '../context';
 
 // TicketGate Component
 export const CheckoutWeb3 = (): JSX.Element => {
 	// tokengate context
+	const { checkoutSelection } = useContext(TokenGateContext);
 	// wallet data hooks
 	const [{ data: accountData, error: accountError, loading: accountLoading }, disconnect] = useAccount();
 
 	// account data present, proceed to rest of checkout
 	// return either checkout confirmation or checkout selection
 	if (accountData){
-		// todo: checkout selection component
 		// checkout selection (choose NFT's / assets to redeem)
-		if (false){
+		if (!checkoutSelection){
 			return <Web3CheckoutSelection accountData={accountData}/>
 		}
 
-		// no checkout selection required, proceed to web3 checkout confirmation
-		return (
-			<Web3CheckoutConfirmation accountData={accountData} disconnect={disconnect}/>
-		)
+		// proceed to web3 checkout confirmation
+		if (checkoutSelection){
+			return (
+				<Web3CheckoutConfirmation accountData={accountData} disconnect={disconnect}/>
+			)
+		}
 	}
 	// account data error
 	if (accountError){

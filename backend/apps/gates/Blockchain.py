5a0472e6b55d8ccd866e13c23eb30a661e42ef28
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-04-23 20:58:40 -0400

    [backend] cleanup gates/Blockchain -remove pydantic classes -streamline function signatures

--- backend/apps/gates/Blockchain.py
+++ backend/apps/gates/Blockchain.py
@@ -3,39 +3,64 @@ from typing import List, Optional, Union
 
 import requests
 from eth_account.messages import encode_defunct
-from pydantic import BaseModel
 from pytz import utc
 from web3.auto import w3
 
-
 #
-# SCHEMAS
+# MORALIS FUNCTIONS
 #
-class Requirement(BaseModel):
-    amount: int
-    blockchain: str = "EVM"
-    chain_id: Optional[int] = 1
-    asset_type: Optional[str]
-    asset_address: Optional[str]
-    token_id: Optional[List[int]]
-    to_block: Optional[int]
+def moralis_get_fungible(
+    chain_id="", wallet_address="", token_addresses=[], to_block=None
+):
+    """
+    Gets token balances for a specific address
+
+    Gets token owned by the given address, at the given token_addresses, up until to_block
+    """
+    url = f"https://deep-index.moralis.io/api/v2/{wallet_address}/erc20/"
+    payload = {
+        "chain": chain_id,
+        "to_block": to_block,
+        "format": "decimal",
+        "token_addresses": token_addresses,
+    }
+    headers = {
+        "X-API-Key": "UgecTEh53XCmf9sft9ZkcZWH5Bpx0wbglo8TYHfrqb7e0mW2NCtAgjFQ4uEKT6V4"
+    }
+    r = requests.get(url, params=payload, headers=headers)
+    return r.json()
 
 
-class SelectedAsset(BaseModel):
-    asset_address: str #token_address
-    asset_type: str #contract_type
-    metadata: str #metadata
-    name: str #name
-    token_id: str #token_id
-    token_uri: str #token_uri
+def moralis_get_nfts(chain_id="", wallet_address="", token_address=""):
+    """
+    Gets NFTs owned by the given address, at the given token_address
+
+    Use the token_address param to get results for a specific contract only
+    Note results will include all indexed NFTs.
+
+    Any request which includes the token_address param
+    will start the indexing process for that NFT collection
+    the very first time it is requested
+    """
+    url = f"https://deep-index.moralis.io/api/v2/{wallet_address}/nft/{token_address}"
+    payload = {"chain": chain_id, "format": "decimal"}
+    headers = {
+        "X-API-Key": "UgecTEh53XCmf9sft9ZkcZWH5Bpx0wbglo8TYHfrqb7e0mW2NCtAgjFQ4uEKT6V4"
+    }
+    r = requests.get(url, params=payload, headers=headers)
+    if r.status_code == 200:
+        json = r.json()
+        if json['total'] > 0:
+            return json['result']
+
+    return []
+
 
 #
 # Public Utilities class
 #
 class Utilities:
-    def validate_signature(
-        signature=None, signed_message="", address="", tokengate_id=""
-    ):
+    def validate_signature(signature=None, signed_message="", address="", tokengate_id=""):
         """
         Reusable method to validate a given signature
         """
@@ -65,53 +90,42 @@ class Utilities:
 
         return True, "OK"
 
-    def fetch_options_against_requirements(
-        wallet_address: str, requirements: List[Requirement]
-    ):
+    def fetch_options_against_requirements(wallet_address="", requirements=[]):
         """
         Return list of requirements, each augmented with available asset options
         """
         requirements_with_options = []
-        # loop over requirements
         for requirement in requirements:
             # fungible
             if requirement["asset_type"] == "ERC20":
-                print("fungible")
-                requirements_with_options.append(
-                    {
-                        "requirement": requirement,
-                        "options": moralis_get_fungible(
-                            chain_id=hex(requirement["chain_id"]),
-                            wallet_address=wallet_address,
-                            token_addresses=requirement["asset_address"],
-                            to_block=requirement["to_block"],
-                        ),
-                    }
-                )
+                requirements_with_options.append({
+                    "requirement": requirement,
+                    "options": moralis_get_fungible(
+                        chain_id=hex(requirement["chain_id"]),
+                        wallet_address=wallet_address,
+                        token_addresses=requirement["asset_address"],
+                        to_block=requirement["to_block"],
+                    )
+                })
             # non fungible
-            if (
-                requirement["asset_type"] == "ERC721"
-                or requirement["asset_type"] == "ERC1155"
-            ):
-                print("nonfungible")
-                requirements_with_options.append(
-                    {
-                        "requirement": requirement,
-                        "options": moralis_get_nfts(
-                            chain_id=hex(requirement["chain_id"]),
-                            wallet_address=wallet_address,
-                            token_address=requirement["asset_address"],
-                        ),
-                    }
-                )
+            if (requirement["asset_type"] == "ERC721" or requirement["asset_type"] == "ERC1155"):
+                requirements_with_options.append({
+                    "requirement": requirement,
+                    "options": moralis_get_nfts(
+                        chain_id=hex(requirement["chain_id"]),
+                        wallet_address=wallet_address,
+                        token_address=requirement["asset_address"],
+                    )
+                })
 
         return requirements_with_options
 
+
     def validate_options_against_requirements(
-        wallet_address: str,
-        limit_per_person: int,
-        requirements: List[Requirement],
-        requirements_with_options: List[Union[Requirement, SelectedAsset]]
+        wallet_address="",
+        limit_per_person=1,
+        requirements=[],
+        requirements_with_options=[]
     ):
         """
         Validate options against given options (union of requirement ++ selected asset
@@ -143,53 +157,3 @@ class Utilities:
 
         # only return if all options succeed
         return True, verified_options
-
-
-#
-# MORALIS FUNCTIONS
-#
-def moralis_get_fungible(
-    chain_id: str, wallet_address: str, token_addresses: List[str], to_block: int
-):
-    """
-    Gets token balances for a specific address
-
-    Gets token owned by the given address, at the given token_addresses, up until to_block
-    """
-    url = f"https://deep-index.moralis.io/api/v2/{wallet_address}/erc20/"
-    payload = {
-        "chain": chain_id,
-        "to_block": to_block,
-        "format": "decimal",
-        "token_addresses": token_addresses,
-    }
-    headers = {
-        "X-API-Key": "UgecTEh53XCmf9sft9ZkcZWH5Bpx0wbglo8TYHfrqb7e0mW2NCtAgjFQ4uEKT6V4"
-    }
-    r = requests.get(url, params=payload, headers=headers)
-    return r.json()
-
-
-def moralis_get_nfts(chain_id: str, wallet_address: str, token_address: str):
-    """
-    Gets NFTs owned by the given address, at the given token_address
-
-    Use the token_address param to get results for a specific contract only
-    Note results will include all indexed NFTs.
-
-    Any request which includes the token_address param
-    will start the indexing process for that NFT collection
-    the very first time it is requested
-    """
-    url = f"https://deep-index.moralis.io/api/v2/{wallet_address}/nft/{token_address}"
-    payload = {"chain": chain_id, "format": "decimal"}
-    headers = {
-        "X-API-Key": "UgecTEh53XCmf9sft9ZkcZWH5Bpx0wbglo8TYHfrqb7e0mW2NCtAgjFQ4uEKT6V4"
-    }
-    r = requests.get(url, params=payload, headers=headers)
-    if r.status_code == 200:
-        json = r.json()
-        if json['total'] > 0:
-            return json['result']
-
-    return []

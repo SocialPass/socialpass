89d04162d8a2b31e38fdfb8bb577c01f7f59662d
Nicolas Ryberg <nico.ryberg@gmail.com>
2022-05-14 20:36:21 -0300

    Code clean-up

--- backend/apps/dashboard/forms.py
+++ backend/apps/dashboard/forms.py
@@ -53,19 +53,3 @@ class TicketGateForm(forms.ModelForm):
             }),
             "requirements": forms.HiddenInput(),
         }
-        # widgets = {
-        #     'email': forms.EmailInput(attrs={
-        #     'class': 'form-control',
-        #     'style': 'background-color: black',
-        #     'placeholder': 'Your Email',
-        #     }),
-        #     'name': forms.TextInput(attrs={
-        #     'class': 'form-control',
-        #     'style': 'background-color: black',
-        #     'placeholder': 'Your Name'}),
-        #     'text': forms.Textarea(attrs={
-        #     'rows': 3,
-        #     'style': 'background-color: black',
-        #     'class': 'form-control',
-        #     'placeholder': 'Hello Roylan,'})
-        #  }

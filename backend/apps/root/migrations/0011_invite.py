a0e01be725dba1d398f7ac56f97307d905cd2604
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-05-17 19:03:41 -0400

    bugfix: remove nonexistent parent migration reference

--- backend/apps/root/migrations/0011_invite.py
+++ backend/apps/root/migrations/0011_invite.py
@@ -7,7 +7,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('invitations', '0005_alter_invitation_id'),
         ('root', '0010_rename_price_per_capacity_pricingrule_price_per_ticket_and_more'),
     ]
 

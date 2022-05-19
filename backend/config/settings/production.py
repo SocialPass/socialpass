971f0451bc48f7ab1d1b657b2e5d26294c9de2f6
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-05-18 23:50:15 -0400

    remove redis

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -2,7 +2,6 @@ import logging
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
-from sentry_sdk.integrations.redis import RedisIntegration
 
 from .base import *  # noqa
 from .base import env
@@ -124,7 +123,7 @@ sentry_logging = LoggingIntegration(
     level=SENTRY_LOG_LEVEL,  # Capture info and above as breadcrumbs
     event_level=logging.ERROR,  # Send errors as events
 )
-integrations = [sentry_logging, DjangoIntegration(), RedisIntegration()]
+integrations = [sentry_logging, DjangoIntegration()]
 
 sentry_sdk.init(
     dsn=SENTRY_DSN,

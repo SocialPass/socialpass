d68f00ac693a64c9d9886722e03445c04c3b72a1
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-05-26 23:12:23 -0400

    temp: remove api_eventportal refactored views

--- backend/apps/api_eventportal/views.py
+++ backend/apps/api_eventportal/views.py
@@ -32,16 +32,12 @@ class TicketedEventRetrieve(RetrieveAPIView):
     permission_classes = [AllowAny]
 
 
-class TicketedEventRequestAccess(SetTicketedEvent, CreateAPIView):
+class TicketedEventRequestAccess(SetTicketedEvent, APIView):
     """
     Creates & returns one-time Signature model for an EVM client to sign.
     This signature is used as authentication in further views for accessing a Ticketed Event
     """
-    serializer_class = serializers.SignatureSerializer
-
-    def perform_create(self, serializer, *args, **kwargs):
-        self.set_ticketed_event(public_id=self.kwargs["public_id"])
-        serializer.save(ticketed_event=self.ticketed_event)
+    pass
 
 
 class TicketedEventVerifyAccess(SetTicketedEvent, APIView):
@@ -53,36 +49,7 @@ class TicketedEventVerifyAccess(SetTicketedEvent, APIView):
 
     Note: Uses signature.unique_code as authentication (check for verified & redeemed)
     """
-    def post(self, request, *args, **kwargs):
-        # set ticketed_event
-        self.set_ticketed_event(public_id=self.kwargs["public_id"])
-
-        # Serialize data
-        serialized = serializers.TicketedEventVerifyAccessSerializer(data=request.data)
-        serialized.is_valid(raise_exception=True)
-
-        # Get signature from data
-        try:
-            self.signature = Signature.objects.get(ticketed_event=self.ticketed_event, unique_code=serialized.data.get("signature_id"))
-        except Exception:
-            raise Http404
-
-        # Validate signature
-        signature_success, signature_msg = blockchain_service.verify_signature(
-            signature=self.signature,
-            wallet_address=serialized.data.get("wallet_address"),
-            signed_message=serialized.data.get("signed_message"),
-            ticketed_event_id=self.ticketed_event.public_id,
-        )
-        if not signature_success:
-            return Response(signature_msg, status=401)
-
-        # TODO: return ticket options based on asset ownership of wallet_address
-        ticketing_service.get_ticket_options(
-            ticketed_event=self.ticketed_event,
-            wallet_address=serialized.data.get("wallet_address")
-        )
-        return Response("OK")
+    pass
 
 
 class TicketedEventIssueTickets(SetTicketedEvent, APIView):

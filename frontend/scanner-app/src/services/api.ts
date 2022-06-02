cfb17800e15cbba9f0185682368c85efabdbfe1a
Pedro Vieira <mvpedro.cc@gmail.com>
2022-06-02 16:40:10 -0300

    Creating constant for easier API redirecting

--- frontend/scanner-app/src/services/api.ts
+++ frontend/scanner-app/src/services/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
+import { BASEURL } from '../utils/constants'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: BASEURL,
   timeout: 100000,
 })
+

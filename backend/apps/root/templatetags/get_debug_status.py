b0e9f3fb639019b50a102a70b957e401a601ee6c
Tahmid Khan <tahmid.hm.dev@gmail.com>
2022-06-07 19:07:38 +0600

    Simplified debug template tag

--- backend/apps/root/templatetags/get_debug_status.py
+++ backend/apps/root/templatetags/get_debug_status.py
@@ -6,11 +6,4 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def get_debug_status(context):
-	request = context["request"]
-	host = request.get_host()
-
-	for ip in settings.INTERNAL_IPS:
-		if host.startswith(ip):
-			return True
-			
-	return False
+	return settings.DEBUG

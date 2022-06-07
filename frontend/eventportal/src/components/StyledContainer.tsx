1b68ed53fd4f3ee584acd9c8141c33fca403dd52
lucasmv2205 <lmartins2205@gmail.com>
2022-06-07 08:58:46 -0300

    update checkout status page to match with figma

--- frontend/eventportal/src/components/StyledContainer.tsx
+++ frontend/eventportal/src/components/StyledContainer.tsx
@@ -18,48 +18,50 @@ export const StyledContainer = ({
   const navigate = useNavigate();
   return (
     <div className="left-30 container-fluid p-0 card">
-      {location.pathname.length > 45 ? (
-        <header
-          className="small-header"
-          style={{ backgroundImage: `url(${headerImage})` }}
-        >
-          <div className="back" onClick={() => navigate(-1)}>
-            <img src={_backButton} alt="Back Button" height="24" width="24" />
-            <h4 className="ps-5 m-0">Go back</h4>
-          </div>
-        </header>
-      ) : (
-        <header
-          className="header d-flex align-items-start justify-content-center"
-          style={{ backgroundImage: `url(${headerImage})` }}
-        >
-          <div className="team-info">
-            <img
-              src={
-                retrieveJson &&
-                retrieveJson.organizer_info &&
-                retrieveJson.organizer_info.profile_image
-              }
-              alt="Team Image"
-            />
-            <h4>
-              {retrieveJson &&
-                retrieveJson.organizer_info &&
-                retrieveJson?.organizer_info.name}
-            </h4>
-          </div>
-        </header>
-      )}
-      <div className="d-flex mx-3 mx-md-5 mt-5 flex-grow-1 h-100 p-30">
-        {children}
+      <div>
+        {location.pathname.length > 45 ? (
+          <header
+            className="small-header"
+            style={{ backgroundImage: `url(${headerImage})` }}
+          >
+            <div className="back" onClick={() => navigate(-1)}>
+              <img src={_backButton} alt="Back Button" height="24" width="24" />
+              <h4 className="ps-5 m-0">Go back</h4>
+            </div>
+          </header>
+        ) : (
+          <header
+            className="header d-flex align-items-start justify-content-center"
+            style={{ backgroundImage: `url(${headerImage})` }}
+          >
+            <div className="team-info">
+              <img
+                src={
+                  retrieveJson &&
+                  retrieveJson.organizer_info &&
+                  retrieveJson.organizer_info.profile_image
+                }
+                alt="Team Image"
+              />
+              <h4>
+                {retrieveJson &&
+                  retrieveJson.organizer_info &&
+                  retrieveJson?.organizer_info.name}
+              </h4>
+            </div>
+          </header>
+        )}
+        <div className="d-flex mx-3 mx-md-5 mt-5 flex-grow-1 h-100 p-30">
+          {children}
+        </div>
+        <footer className="d-flex flex-row justify-content-between align-items-center">
+          <img src={FAQImage} alt="image" />
+          <small className="d-flex flex-row align-items-center">
+            Powered by &nbsp;
+            <img src={logoImage} alt="image" />
+          </small>
+        </footer>
       </div>
-      <footer className="d-flex flex-row justify-content-between align-items-center">
-        <img src={FAQImage} alt="image" />
-        <small className="d-flex flex-row align-items-center">
-          Powered by &nbsp;
-          <img src={logoImage} alt="image" />
-        </small>
-      </footer>
     </div>
   );
 };
--- frontend/eventportal/src/pages/CheckoutStatus.tsx
+++ frontend/eventportal/src/pages/CheckoutStatus.tsx
@@ -7,47 +7,49 @@ import correct from "../static/images/icons/correct.svg";
 import denied from "../static/images/icons/denied.svg";
 
 export const CheckoutStatus = () => {
-  const { id, grantAccessJson, grantAccessError, retrieveJson } = useContext(EventPortalContext);
+  const { id, grantAccessJson, grantAccessError, retrieveJson } =
+    useContext(EventPortalContext);
   const navigate = useNavigate();
   console.log(grantAccessJson, grantAccessError);
-  if (grantAccessJson) {
+  if (!grantAccessJson) {
     return (
       <div className="row flex-grow-1 m-0 mt-3 align-items-center">
         <div className="col-md-7 mb-4 d-flex">
           <div className="col col-md-10">
             <h1>Congrats</h1>
             <p>You made it! Click the button to download your ticket</p>
-            <div className="bg-success p-5 strong my-15 text-center d-flex align-items-center justify-content-evenly">
+            <div className="fw-bold bg-success p-5 strong my-15 text-center d-flex align-items-center justify-content-evenly">
               <img src={correct} height="16.9" width="16.9" className="me-4" />
               access granted
               <span></span>
             </div>
-            <div className="d-flex flex-row justify-content-around">
+            <div className="d-flex flex-row justify-content-start align-items-center gap-30">
               <img
                 className="avatar-img"
-                src="https://resultadosdigitais.com.br/files/2015/08/thestocks-imagem.jpg"
+                src={retrieveJson.organizer_info.profile_image}
               />
               <div>
-                <strong>{retrieveJson.title}</strong>
+                <strong>{retrieveJson.event_info.title}</strong>
                 <p className="d-flex align-items-center m-0 mt-1 mb-1">
                   <img
                     src={clock}
                     height="16.9"
                     width="16.9"
-                    className="me-1"
+                    className="me-5"
                     alt="Date & Time"
                   />
-                  {retrieveJson.date} | {retrieveJson.timezone}
+                  {retrieveJson.event_info.date} |{" "}
+                  {retrieveJson.event_info.timezone}
                 </p>
                 <p className="d-flex align-items-center m-0 mt-1 mb-1">
                   <img
                     src={location}
                     height="16.9"
                     width="16.9"
-                    className="me-1"
+                    className="me-5"
                     alt="Date & Time"
                   />
-                  {retrieveJson.location}
+                  {retrieveJson.event_info.location}
                 </p>
               </div>
             </div>
@@ -65,37 +67,38 @@ export const CheckoutStatus = () => {
         <div className="col col-md-10">
           <h1>Something Went Wrong</h1>
           <p>We’re sorry, we were not able to redeem this Token Gate</p>
-          <div className="bg-denied p-5 strong my-15 text-center d-flex align-items-center justify-content-evenly">
+          <div className="fw-bold bg-denied p-5 strong my-15 text-center d-flex align-items-center justify-content-evenly">
             <img src={denied} height="16.9" width="16.9" />
             access denied
             <span></span>
           </div>
-          <div className="d-flex flex-row justify-content-around">
+          <div className="d-flex flex-row justify-content-around align-items-center">
             <img
               className="avatar-img"
-              src="https://resultadosdigitais.com.br/files/2015/08/thestocks-imagem.jpg"
+              src={retrieveJson.organizer_info.profile_image}
             />
             <div>
-              <strong>{retrieveJson.title}</strong>
+              <strong>{retrieveJson.event_info.title}</strong>
               <p className="d-flex align-items-center m-0 mt-1 mb-1">
                 <img
                   src={clock}
                   height="16.9"
                   width="16.9"
-                  className="me-1"
+                  className="me-5"
                   alt="Date & Time"
                 />
-                {retrieveJson.date} | {retrieveJson.timezone}
+                {retrieveJson.event_info.date} |{" "}
+                {retrieveJson.event_info.timezone}
               </p>
               <p className="d-flex align-items-center m-0 mt-1 mb-1">
                 <img
                   src={location}
                   height="16.9"
                   width="16.9"
-                  className="me-1"
+                  className="me-5"
                   alt="Date & Time"
                 />
-                {retrieveJson.location}
+                {retrieveJson.event_info.location}
               </p>
             </div>
           </div>
@@ -103,7 +106,7 @@ export const CheckoutStatus = () => {
       </div>
       <div className="col-md-5">
         <button
-          className="btn-primary"
+          className="btn-primary fw-700 fs-20"
           onClick={() => {
             navigate(`/${id}`);
           }}

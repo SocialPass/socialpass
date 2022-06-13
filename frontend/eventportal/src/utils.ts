1fa36b37f35610d1e03ea0ca28d0024f5ae11b03
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-06-12 20:04:48 -0400

    bugfix: replace MAYC placeholder with actual blockchain requirements

---
+++ frontend/eventportal/src/utils.ts
@@ -0,0 +1,5 @@
+ export const truncateAddress = (address:string) => {
+	const beginning = address.substring(0, 7);
+	const end = address.slice(address.length - 7);
+	return `${beginning}...${end}`
+  }

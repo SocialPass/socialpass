2052c77be8b7c8b7050d6deaf3c7772d1f0e8706
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-06-12 22:25:40 -0400

    bump wagmi>=0.3

--- frontend/eventportal/src/pages/CheckoutWeb3.tsx
+++ frontend/eventportal/src/pages/CheckoutWeb3.tsx
@@ -10,7 +10,9 @@ import infoButton from "../static/images/icons/infoButton.svg";
 // ConnectorWallets
 // Return UI for wallet connectors
 export const CheckoutWeb3 = () => {
+  const navigate = useNavigate();
   const [selectedWallet, setSelectedWallet] = useState<any>();
+  const [loadingText, setLoadingText] = useState<any>('Loading...');
   const [statusButton, setStatusButton] = useState<any>(true);
   const {
     id,
@@ -21,14 +23,12 @@ export const CheckoutWeb3 = () => {
     setGrantAccessJson,
     setGrantAccessError,
   } = useContext(EventPortalContext);
-  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
-  const [{ data: connectData, error: connectError }, connect] = useConnect();
-  const [{ data: accountData }] = useAccount();
-  const [{ data: signData, loading: signLoading }, signMessage] =
-    useSignMessage({
-      message: requestAccessJson?.signing_message,
-    });
+  const connectHook = useConnect();
+  const accountHook = useAccount();
+  const signHook = useSignMessage({
+    message: requestAccessJson?.signing_message,
+  })
 
   // request access handler (based on web3 account data change)
   useEffect(() => {
@@ -55,14 +55,15 @@ export const CheckoutWeb3 = () => {
   // handles signing message and posting related data to API
   useEffect(() => {
     (async function () {
-      if (signData) {
+      if (signHook.data) {
         setLoading(true);
+        setLoadingText('Verifying ownership')
         let response;
         response = await TicketedEventGrantAccess.call({
           public_id: id,
           checkout_type: "blockchain_ownership",
-          wallet_address: accountData?.address,
-          signed_message: signData,
+          wallet_address: accountHook?.data?.address,
+          signed_message: signHook.data,
           blockchain_ownership_id: requestAccessJson.id,
           tickets_requested: generalAdmissionSelect,
         });
@@ -78,113 +79,120 @@ export const CheckoutWeb3 = () => {
         }
       }
     })();
-  }, [signData]);
+  }, [signHook]);
 
   function handleNavigateBack() {
     navigate(-1);
   }
 
-  function handleConnectWallet(x: any) {
+  function handleSelectWallet(x: any) {
     setSelectedWallet(x);
-    connect(x);
     setStatusButton(false);
   }
 
-  function handleCheckout() {
-    console.log("checkout...");
-    signMessage();
+  async function handleCheckout() {
+    setLoadingText("Awaiting wallet connection");
+    try {
+      await connectHook.connectAsync(selectedWallet);
+    }
+    catch(err) {
+      console.log(err);
+    }
+    if (accountHook.data?.address){
+      setLoadingText(`Awaiting wallet signature`);
+      await signHook.signMessageAsync();
+    }
   }
 
-  if (signLoading || loading) {
-    return <Loading loadingText="Verifying NFTs" />;
+  if (signHook.isLoading || accountHook.isLoading || connectHook.isConnecting || loading) {
+    return <Loading loadingText={loadingText}/>;
   }
 
-  if (connectData) {
-    return (
-      <div className="responsive-page-selection">
-        <div className="d-flex flex-column">
-          <div>
-            <h3 className="fs-20">Completer Checkout</h3>
-            <p>Select from one of the checkout options below</p>
-          </div>
-          <div className="d-flex flex-row align-items-start justify-content-between">
-            <div>
-              <div className="d-flex flex-row align-items-center me-15">
-                <span className="fs-18 fw-bold me-15">Checkout option</span>
-              </div>
-            </div>
-          </div>
-          <div className="d-flex flex-row justify-content-start align-items-center mt-15 gap-30">
-            <div className="border-color-primary px-30 py-10 rounded-3 d-flex align-items-start justify-content-start flex-column">
-              <div className="fw-bold fs-15">Proof of NFT Ownership</div>
-              <span>Select from the crypto wallet options</span>
-            </div>
-
-            <div className="tooltip">
-              <img src={infoButton} />
-              <div className="right">
-                <span className="tooltip-text fs-12">
-                  Proof of ownership is not an NFT trade. We need to prove you
-                  own the NFT in order to get the ticket.
-                </span>
-                <i></i>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-12 d-flex mt-10 d-flex gap-10 column-display-mobile">
-            {connectData.connectors.map((x) => (
-              <button
-                className={
-                  selectedWallet === x
-                    ? "fs-12 fw-bold card-active shadow-none d-flex flex-column align-items-center justify-content-around w-100 mt-3"
-                    : "btn-secondary fs-12 border-0 card-disabled shadow-none d-flex flex-column align-items-center justify-content-around w-100 mt-3"
-                }
-                disabled={!x.ready}
-                key={x.id}
-                id={x.id}
-                onClick={() => handleConnectWallet(x)}
-              >
-                <Web3ConnectorImage
-                  selectedWallet={selectedWallet}
-                  connector={x.name}
-                />
-                {x.name}
-                {!x.ready && " (unsupported)"}
-              </button>
-            ))}
-          </div>
-          {connectError && (
-            <div>{connectError?.message ?? "Failed to connect"}</div>
-          )}
+  return (
+    <div className="responsive-page-selection">
+      <div className="d-flex flex-column">
+        <div>
+          <h3 className="fs-20">Complete Checkout</h3>
+          <p>Select from one of the checkout options below</p>
         </div>
-        <div className="bg-gray d-flex flex-column justify-start-center">
-          <div className="d-flex flex-column align-items-start justify-start-center p-30">
-            <div className="d-flex align-items-center justify-conent-center">
-              <h3 className="fs-20">Summary</h3>
-              <a
-                onClick={handleNavigateBack}
-                className="ms-15 mt-5 text-edit fs-15 fw-bold"
-              >
-                Edit
-              </a>
-            </div>
-            <div className="d-flex flex-row">
-              <p>{generalAdmissionSelect} X General Admission Ticket</p>
+        <div className="d-flex flex-row align-items-start justify-content-between">
+          <div>
+            <div className="d-flex flex-row align-items-center me-15">
+              <span className="fs-18 fw-bold me-15">Checkout option</span>
             </div>
           </div>
-          <div className="d-flex align-items-center justify-content-center p-30 mt-50">
+        </div>
+        <div className="d-flex flex-row justify-content-start align-items-center mt-15 gap-30">
+          <div className="border-color-primary px-30 py-10 rounded-3 d-flex align-items-start justify-content-start flex-column">
+            <div className="fw-bold fs-15">Proof of NFT Ownership</div>
+            <span>Select from the crypto wallet options</span>
+          </div>
+
+          <div className="tooltip">
+            <img src={infoButton} />
+            <div className="right">
+              <span className="tooltip-text fs-12">
+                Proof of ownership is not an NFT trade. We need to prove you
+                own the NFT in order to get the ticket.
+              </span>
+              <i></i>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-12 d-flex mt-10 d-flex gap-10 column-display-mobile">
+          {connectHook.connectors.map((x) => (
             <button
-              disabled={statusButton}
-              onClick={() => handleCheckout()}
-              className="btn btn-primary fs-20 text-capitalize rounded-3"
+              className={
+                selectedWallet === x
+                  ? "fs-12 fw-bold card-active shadow-none d-flex flex-column align-items-center justify-content-around w-100 mt-3"
+                  : "btn-secondary fs-12 border-0 card-disabled shadow-none d-flex flex-column align-items-center justify-content-around w-100 mt-3"
+              }
+              disabled={!x.ready}
+              key={x.id}
+              id={x.id}
+              onClick={() => handleSelectWallet(x)}
             >
-              Checkout
+              <Web3ConnectorImage
+                selectedWallet={selectedWallet}
+                connector={x.name}
+              />
+              {x.name}
+              {!x.ready && " (unsupported)"}
             </button>
+          ))}
+        </div>
+        {connectHook.error && (
+          <div>{connectHook.error?.message ?? "Failed to connect"}</div>
+        )}
+        {signHook.error && (
+          <div>{signHook.error?.message ?? "Failed to connect"}</div>
+        )}
+      </div>
+      <div className="bg-gray d-flex flex-column justify-start-center">
+        <div className="d-flex flex-column align-items-start justify-start-center p-30">
+          <div className="d-flex align-items-center justify-conent-center">
+            <h3 className="fs-20">Summary</h3>
+            <a
+              onClick={handleNavigateBack}
+              className="ms-15 mt-5 text-edit fs-15 fw-bold"
+            >
+              Edit
+            </a>
           </div>
+          <div className="d-flex flex-row">
+            <p>{generalAdmissionSelect} X General Admission Ticket</p>
+          </div>
+        </div>
+        <div className="d-flex align-items-center justify-content-center p-30 mt-50">
+          <button
+            disabled={statusButton}
+            onClick={() => handleCheckout()}
+            className="btn btn-primary fs-20 text-capitalize rounded-3"
+          >
+            Checkout
+          </button>
         </div>
       </div>
-    );
-  }
-
-  return null;
-};
+    </div>
+  );
+}
---
+++ frontend/eventportal/src/web3/client.tsx
@@ -0,0 +1,44 @@
+import { createClient, configureChains, defaultChains } from 'wagmi'
+
+import { infuraProvider } from 'wagmi/providers/infura'
+import { publicProvider } from 'wagmi/providers/public'
+
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+
+// API key for Ethereum node
+// Two popular services are Infura (infura.io) and Alchemy (alchemy.com)
+const infuraId = '461fc5573eaa427f803fa7e3a5de4139'
+
+const { chains } = configureChains(defaultChains, [
+  infuraProvider({ infuraId }),
+  publicProvider(),
+])
+
+export const web3Client = createClient({
+  autoConnect: true,
+  connectors: [
+	new MetaMaskConnector({ chains }),
+	new CoinbaseWalletConnector({
+	  chains,
+	  options: {
+		appName: 'SocialPass',
+	  },
+	}),
+	new WalletConnectConnector({
+	  chains,
+	  options: {
+		qrcode: true,
+	  },
+	}),
+	new InjectedConnector({
+	  chains,
+	  options: {
+		name: 'Injected',
+		shimDisconnect: true,
+	  },
+	}),
+  ],
+})

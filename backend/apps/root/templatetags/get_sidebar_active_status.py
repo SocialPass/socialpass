e18698dc67e2b6d1032396044bab791c524394d1
Tahmid Khan <tahmid.hm.dev@gmail.com>
2022-06-15 20:28:32 +0600

    Better way to manage sidebar active link (template tag)

---
+++ backend/apps/root/templatetags/get_sidebar_active_status.py
@@ -0,0 +1,26 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def get_sidebar_active_status(context, key):
+	url_name = context["request"].resolver_match.url_name
+
+	if key == "events":
+		if url_name in [
+			"ticketgate_list", "ticketgate_create", "ticketgate_detail",
+			"ticketgate_update", "ticketgate_stats", 
+			"ticketgate_price_estimator", "ticketgate_checkout",
+			"ticketgate_checkout_success_callback",
+			"ticketgate_checkout_failure_callback"
+		]:
+			return "active"
+	elif key == "team-details":
+		if url_name in ["team_detail", "team_update"]:
+			return "active"
+	elif key == "manage-members":
+		if url_name in ["team_members", "team_member_delete"]:
+			return "active"
+
+	return ""

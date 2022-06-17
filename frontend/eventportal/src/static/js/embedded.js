2c4b408f9ad5e26c011cf649d0fd6e6f4f027ddd
lucasmv2205 <lmartins2205@gmail.com>
2022-06-17 13:04:17 -0300

    take off write function

--- frontend/eventportal/src/static/js/embedded.js
+++ frontend/eventportal/src/static/js/embedded.js
@@ -6,18 +6,13 @@ class Frame{
     }
 
     mount(container_tag_id){
-        const app = document.getElementById(container_tag_id)
+        const app = document.getElementById(container_tag_id);
         var ifrm = document.createElement('iframe');
-        console.log("setting iframe")
+        console.log("setting iframe");
         ifrm.setAttribute('id', 'portal');
         ifrm.setAttribute('style', 'width: 100vw; height:100vh; border: none;');
         ifrm.setAttribute('src', this.domain_url + '/' + this.uuid_event);
         console.log(ifrm);
         const tag = `&lt;iframe id="portal" style="width: 100vw; height:100vh; border: none;" src=${`https://tickets.socialpass.io/${this.uuid_event}`}&gt;&lt;/iframe&gt;`;
-        document.write(tag);
-
-
-        // var parent_container = document.getElementById(container_tag_id);
-        // parent_container.appendChild(ifrm);
     }
 }

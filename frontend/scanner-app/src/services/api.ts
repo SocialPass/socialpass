5bdfe57ee1668b311a3c9532b2e1959ee5a442e4
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-06-17 21:17:35 -0400

    cra --> vite

--- frontend/scanner-app/src/services/api.ts
+++ frontend/scanner-app/src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
-import { BASEURL } from '../utils/constants'
+const prodURL = import.meta.env.VITE_APP_API_URL;
+const devURL = "/api";
+const BASEURL = import.meta.env.PROD ? prodURL : devURL;
 
 export const api = axios.create({
   baseURL: BASEURL,

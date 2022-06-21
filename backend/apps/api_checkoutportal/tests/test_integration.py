149060e86514f8ca3228d0cb424f4447ec530104
BeautyFades <fellipedfernandes@gmail.com>
2022-06-20 21:37:58 -0300

    Fixed kwargs input to view

--- backend/apps/api_checkoutportal/tests/test_integration.py
+++ backend/apps/api_checkoutportal/tests/test_integration.py
@@ -20,15 +20,13 @@ class GetEventDetailsTestCase(APITestCase):
         User.objects.create(username='beautyfades', is_staff=True)
         Team.objects.create(name='UgaMaster', description='Rei dos Ugas')
         Event.objects.create(title='uhul', description='uga', date='2022-06-15 04:38:00+00',
-                             timezone='America/Bahia', capacity='1000', team_id='1', user_id='1',
-                             public_id='901b0474-6278-42a1-bf55-618ee69b7402')
+                             timezone='America/Bahia', capacity='1000', team_id='1', user_id='1')
 
         event = Event.objects.last()
-        print(event.public_id)
 
         factory = APIRequestFactory()
         view = EventPortalRetrieve.as_view()
         request = factory.get(f'/event-portal/retrieve/{str(event.public_id)}/')
         print(request)
-        response = view(request)
+        response = view(request, public_id=str(event.public_id))
         self.assertEqual(response.status_code, status.HTTP_200_OK)

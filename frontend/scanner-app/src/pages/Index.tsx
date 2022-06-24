95c73b2b4dc5a7e6bf54aac82c4b7ffc74bbcfdd
lucasmv2205 <lmartins2205@gmail.com>
2022-06-24 17:22:22 -0300

    error page placed in src/pages/Index.tsx

--- frontend/scanner-app/src/pages/Index.tsx
+++ frontend/scanner-app/src/pages/Index.tsx
@@ -1,8 +1,14 @@
-/* eslint-disable jsx-a11y/alt-text */
 export function Index() {
   return (
-    <>
-      Looks like you are lost. Ask the event organizer for your access link.
-    </>
+    <div className="error-footer">
+      <div className="position-absolute top-auto bottom-50">
+        <div className="d-flex flex-column align-items-center justify-content-center gap-30">
+          <span className="error-title fs-20">Looks like you are lost</span>
+          <span className="error-body">
+            Ask the event organizer for your access link.
+          </span>
+        </div>
+      </div>
+    </div>
   );
 }

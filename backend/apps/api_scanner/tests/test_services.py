17a978608c89f17dc2828d77411cd533ee950903
Pedro Vieira <mvpedro.cc@gmail.com>
2022-07-06 10:41:05 -0300

    Pending final tests and cleanup

--- backend/apps/api_scanner/tests/test_services.py
+++ backend/apps/api_scanner/tests/test_services.py
@@ -6,8 +6,6 @@ from apps.root.models import Ticket
 
 class TestScanningService(TestCase):
 
-    # TODO: add ticket fixtures
-
     def test_get_ticket_from_embedded_qr_code(self):
         ticket = Ticket.objects.last()
         if ticket:
@@ -15,3 +13,13 @@ class TestScanningService(TestCase):
                 get_ticket_from_embedded_qr_code(ticket.full_embed).id,
                 ticket.id,
             )
+
+
+    def test_access_key_can_redeem_ticket(self):
+      return "Not yet implemented"  
+
+    def test_redeem_ticket(self):
+        return "Not yet implemented"
+
+    def test_get_claimed_tickets(self):
+        return "Not yet implemented"

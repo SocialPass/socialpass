52ef9fea89ca045bcd662ca68b7d47255a495976
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-07-11 17:48:18 -0400

    fix logo

--- frontend/eventportal/src/components/LargeHeaderContents.tsx
+++ frontend/eventportal/src/components/LargeHeaderContents.tsx
@@ -1,12 +1,12 @@
 import _backButton from "../static/images/back.svg";
-//import socialpassLogo from "../static/images/icons/socialpassLogo.svg";
+import socialpassLogo from "../static/images/icons/socialpassLogo.svg";
 
 export default function LargeHeaderContents() {
 
   return (
     <div>
       <div className="sp-header-logo-img d-flex align-items-center justify-content-center">
-        {/*<img src={socialpassLogo} alt="SocialPass Logo" />*/}
+        <img src={socialpassLogo} alt="SocialPass Logo" />
       </div>
       <div className="team-info-img">
 

6a515abf1134536a7e685e932ac5bb6360997a86
lucasmv2205 <lmartins2205@gmail.com>
2022-07-18 09:41:26 -0300

    useless code

--- frontend/eventportal/src/components/CheckoutSuccessPage.tsx
+++ frontend/eventportal/src/components/CheckoutSuccessPage.tsx
@@ -3,74 +3,8 @@ import { CheckoutPortalContext } from "../context";
 
 export default function CheckoutSuccessPage() {
   const { retrieveJson, grantAccessJson } = useContext(CheckoutPortalContext);
-  //console.log(grantAccessJson, grantAccessError);
 
   return (
-    // <div className="row flex-grow-1 m-0 mt-3 align-items-center">
-    //   <div className="col-md-8 mb-4 d-flex">
-    //     <div className="col col-md-10">
-    //       <h1>Congrats!</h1>
-    //       <p className="fs-16">
-    //         You made it! Click the button to download your tickets:
-    //       </p>
-    //       <div className="fw-bold bg-success p-5 strong my-15 text-center d-flex align-items-center justify-content-evenly">
-    //         <img src={correct} height="16.9" width="16.9" className="me-4" />
-    //         <span className="fs-14">ACCESS GRANTED</span>
-    //       </div>
-    //       <div className="d-flex flex-row justify-content-start align-items-center gap-30">
-    //         <img
-    //           src={retrieveJson.team.image}
-    //           key={retrieveJson.team.image}
-    //           className="avatar-img"
-    //           // src={retrieveJson.team.image}
-    //         />
-    //         <div className="d-flex flex-column gap-5 mb-15">
-    //           <strong className="fs-22">{retrieveJson.title}</strong>
-    //           <p className="d-flex align-items-center m-0 mt-1 mb-1 fs-12">
-    //             <img
-    //               src={clock}
-    //               height="16.9"
-    //               width="16.9"
-    //               className="me-5"
-    //               alt="Date & Time"
-    //             />
-    //             {retrieveJson.start_date}
-    //           </p>
-    //           <p className="d-flex align-items-center m-0 mt-1 mb-1 fs-12">
-    //             <img
-    //               src={timezone}
-    //               height="16.9"
-    //               width="16.9"
-    //               className="me-5"
-    //               alt="Timezone"
-    //             />
-    //             {retrieveJson.timezone}
-    //           </p>
-    //           <p className="d-flex align-items-center m-0 mt-1 mb-1 fs-12">
-    //             <img
-    //               src={location}
-    //               height="16.9"
-    //               width="16.9"
-    //               className="me-5"
-    //               alt="Location"
-    //             />
-    //             {retrieveJson?.location}
-    //           </p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <div className="col-md-4 mt-30">
-    //     <button
-    //       onClick={() =>
-    //         window.open(grantAccessJson[0]["temporary_download_url"])
-    //       }
-    //       className="btn-primary fs-22 mt-5"
-    //     >
-    //       View Tickets
-    //     </button>
-    //   </div>
-    // </div>
     <div className="row">
       {/* <!-- Status information start --> */}
       <div className="col-md-7">

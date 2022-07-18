647e11923312b2fba4dd9cd8a30ca3a39c0b2eb6
lucasmv2205 <lmartins2205@gmail.com>
2022-07-18 11:40:36 -0300

    footer sitcks to the bottom

--- frontend/eventportal/src/components/Footer.tsx
+++ frontend/eventportal/src/components/Footer.tsx
@@ -1,7 +1,6 @@
-
 export default function Footer() {
   return (
-    <div className="content d-flex align-items-center">
+    <div className="content d-flex align-items-center mt-auto">
       <i className="fa-regular fa-question-circle fs-base-p4 text-secondary"></i>
       <span className="ms-auto text-muted">
         &copy; 2022, SP Tech Inc.

9860b6f96f1ff97a12d0666280457ca6a3e0ac0f
lucasmv2205 <lmartins2205@gmail.com>
2022-07-28 20:15:35 -0300

    add to wallet buttons and icons

---
+++ frontend/eventportal/src/components/WalletButtons/AppleWalletButton.tsx
@@ -0,0 +1,16 @@
+import appleWalletIcon from "../../static/images/icons/aw-icon.png";
+
+export default function AppleWalletButton() {
+  return (
+    <button className="btn btn-apple-wallet d-flex align-items-center justify-content-start gap-5 mt-15">
+      <img
+        className="wallet-icon"
+        src={appleWalletIcon}
+        alt="apple wallet icon"
+      />
+      <div>
+        <span className="d-block fs-12 text-start">Add to</span> Apple Wallet
+      </div>
+    </button>
+  );
+}
---
+++ frontend/eventportal/src/components/WalletButtons/GoogleWalletButton.tsx
@@ -0,0 +1,16 @@
+import googleWalletIcon from "../../static/images/icons/gw-icon.svg";
+
+export default function GoogleWalletButton() {
+  return (
+    <button className="btn btn-google-wallet d-flex align-items-center justify-content-start gap-5 mt-15">
+      <img
+        className="wallet-icon"
+        src={googleWalletIcon}
+        alt="google wallet icon"
+      />
+      <div>
+        <span className="d-block fs-12 text-start">Add to</span> Google Wallet
+      </div>
+    </button>
+  );
+}

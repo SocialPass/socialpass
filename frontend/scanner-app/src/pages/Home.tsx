2d4aa5b0fa003a61e40068fc5d847272e2f0be74
mvpedro <mvpedro.cc@gmail.com>
2022-08-01 10:29:21 -0300

    Fixing disparities between backend and frontend static files

--- frontend/scanner-app/src/pages/Home.tsx
+++ frontend/scanner-app/src/pages/Home.tsx
@@ -7,7 +7,7 @@ export function Home() {
   const { eventData }: any = useEvent();
   return (
     <>
-      <div className="main-bg">
+      <div className="socialpass-scanner-main-bg">
       <div className="landing-page-body p-10 py-10">
         <div className="d-flex flex-column align-items-center justify-content-around">
           <img id="SocialPassHeaderIcon" className="p-30" src={window.icon} style={{maxHeight:"250px"}} />

1548879d5570fa763ec092353565a1819ec10208
lucasmv2205 <lmartins2205@gmail.com>
2022-08-10 11:00:46 -0300

    fix errors

--- frontend/eventportal/src/components/LargeHeaderContents.tsx
+++ frontend/eventportal/src/components/LargeHeaderContents.tsx
@@ -3,19 +3,6 @@ import { CheckoutPortalContext } from '../context'
 import _backButton from '../static/images/back.svg'
 
 export default function LargeHeaderContents() {
-<<<<<<< HEAD
-  const { retrieveJson } = useContext(CheckoutPortalContext);
-  let coverImage = retrieveJson?.cover_image
-    ? retrieveJson?.cover_image
-    : "https://images.pexels.com/photos/801863/pexels-photo-801863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-
-  return (
-    <>
-      <div id="large-header" className="w-100 hs-200 position-relative">
-        {/* <!-- Event cover image start --> */}
-        <div className="d-flex align-items-center justify-content-center w-100 h-100 bg-gray-very-light-lm bg-darkgray-very-dim-dm rounded-top overflow-hidden pe-none">
-          <img src={coverImage} className="w-100 h-auto" alt="Cover image" />
-=======
   const { retrieveJson } = useContext(CheckoutPortalContext)
   const coverImage = retrieveJson?.cover_image
     ? retrieveJson?.cover_image
@@ -27,27 +14,17 @@ export default function LargeHeaderContents() {
         {/* <!-- Event cover image start --> */}
         <div className='d-flex align-items-center justify-content-center w-100 h-100 bg-gray-very-light-lm bg-darkgray-very-dim-dm rounded-top overflow-hidden pe-none'>
           <img src={coverImage} className='w-100 h-auto' alt='Cover image' />
->>>>>>> 4920ef621a88322afd6c777d8a2b9b0b87c7afac
         </div>
         {/* <!-- Event cover image end --> */}
 
         {/* <!-- Team image start --> */}
         {retrieveJson?.team.image && retrieveJson.show_team_image && (
-<<<<<<< HEAD
-          <div className="position-absolute z-1 top-100 start-0 translate-y-middle px-content">
-            <div className="ws-100 hs-100 rounded-circle border border-5 border-blend d-flex align-items-center justify-content-center overflow-hidden">
-              <img
-                src={retrieveJson?.team.image}
-                className="d-block w-100 h-auto"
-                alt="Team image"
-=======
           <div className='position-absolute z-1 top-100 start-0 translate-y-middle px-content'>
             <div className='ws-100 hs-100 rounded-circle border border-5 border-blend d-flex align-items-center justify-content-center overflow-hidden'>
               <img
                 src={retrieveJson?.team.image}
                 className='d-block w-100 h-auto'
                 alt='Team image'
->>>>>>> 4920ef621a88322afd6c777d8a2b9b0b87c7afac
               />
             </div>
           </div>
--- frontend/eventportal/src/pages/CheckoutStatus.tsx
+++ frontend/eventportal/src/pages/CheckoutStatus.tsx
@@ -6,13 +6,9 @@ import CheckoutFailedPage from '../components/CheckoutFailedPage'
 export const CheckoutStatus = () => {
   const { grantAccessJson } = useContext(CheckoutPortalContext)
 
-<<<<<<< HEAD
   if (!grantAccessJson) {
     return <CheckoutSuccessPage />;
-=======
-  if (grantAccessJson) {
-    return <CheckoutSuccessPage />
->>>>>>> 4920ef621a88322afd6c777d8a2b9b0b87c7afac
   }
+
   return <CheckoutFailedPage />
 }

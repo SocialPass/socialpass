8b06b66e7c30d52320b32d8c1ab4f1279198e295
Huberto Kaiser Filho <hubertokf@gmail.com>
2022-08-17 13:29:40 -0300

    Improving PDF template, including QR code generator and setting template context

--- backend/apps/root/utilities/PDFTicket.py
+++ backend/apps/root/utilities/PDFTicket.py
@@ -1,12 +1,14 @@
 # find the template and render it.
 from io import BytesIO
 import os
+import base64
 
 from django.conf import settings
 from django.contrib.staticfiles import finders
 from django.template.loader import get_template
 
 from xhtml2pdf import pisa
+import qrcode
 
 from apps.root.models import Ticket
 
@@ -18,7 +20,34 @@ class PDFTicket:
     def set_template(self, template: str):
         self.template = get_template(template)
 
-    def generate_pdf(self, context: dict = {}):
+    def generate_pdf(
+        self,
+        event_title: str,
+        order_number: str,
+        ticket_quantity: int,
+        ticket_type: str,
+        location_name: str,
+        location_address: str,
+        event_date: str,
+        event_time: str,
+        event_timezone: str,
+        barcode_content: str,
+        team_logo_url: str,
+    ) -> BytesIO:
+        """
+        Generate a PDF from the supplied data
+        """
+
+        # Generate the qr code image
+        barcode_image = self._generate_qr_code(barcode_content)
+
+        # Set the template context from the supplied data
+        context = {
+            **locals(),
+            "barcode_image": base64.b64encode(barcode_image.getvalue())
+        }
+
+        # Render the template with the context
         html = self.template.render(context)
 
         # create BytesIO object to store the PDF in memory
@@ -36,9 +65,58 @@ class PDFTicket:
         return pdfFile
 
     def generate_pdf_from_ticket(self, ticket: Ticket):
-        return self.generate_pdf({"ticket": ticket})
+        """
+        Generate a PDF from the ticket object
+        """
+        event = ticket.event
+        team = ticket.event.team
 
-    def _link_callback(uri, rel):
+        # Create the address from event data
+        address = ""
+        if event.address_1:
+            address += event.address_1 + "\n"
+        if event.address_2:
+            address += event.address_2 + "\n"
+
+        address_items_list = []
+        if event.city:
+            address_items_list.append(event.city)
+        if event.region:
+            address_items_list.append(event.region)
+        if event.postal_code:
+            address_items_list.append(event.postal_code)
+        if event.country:
+            address_items_list.append(event.country)
+
+        context = {
+            "event_title": event.title,
+            "order_number": "54593405723",
+            "ticket_quantity": 1,
+            "ticket_type": "General Admission",
+            "location_name": event.location,
+            "location_address": ", ".join(address_items_list),
+            "event_date": event.start_date.strftime("%A, %B %d, %Y"),
+            "event_time": event.start_date.strftime("%I:%M %p"),
+            "event_timezone": event.timezone,
+            "barcode_content": ticket.embed_code,
+            "team_logo_url": team.image.url
+        }
+
+        return self.generate_pdf(**context)
+
+    def _generate_qr_code(self, content) -> BytesIO:
+        """
+        Generate qr code image (PNG) buffer from the supplied content.
+        """
+
+        qrcode_image = qrcode.make(content)
+
+        image_buffer = BytesIO()
+        qrcode_image.save(image_buffer, format="PNG")
+
+        return image_buffer
+
+    def _link_callback(self, uri, rel):
         """
         Convert HTML URIs to absolute system paths so xhtml2pdf can access those
         resources

5b3a371d58218859ae2e0d803cd7ca99c1c6ed7c
joaovictorass95 <joaovictorass95@gmail.com>
2022-08-17 10:13:28 -0300

    fix E501 flake8 error

--- backend/apps/root/utilities/GoogleTicket.py
+++ backend/apps/root/utilities/GoogleTicket.py
@@ -20,13 +20,13 @@ class GoogleTicket:
             "type": "service_account",
             "project_id": "socialpass-358508",
             "private_key_id": "eb6ee15b2777763f54a43ffd62bc2825017d4ff6",
-            "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC3t0XFBeg8CCEs\nHsL0ofIGHjYT6pW2D4SU9HCByNg7FAiqTsbrFEcAQ7Va4sWUS80z0yQaDeA5rJDX\nwYFGCNRvTf0vl7xBptN/BHlg+ZNrzfa95DBgMvDiOj9jOdUwmzs6zRTlHejN4suX\nLRWeRpZB+WRBf4auAtgjzTr63BNaJ6Spzoc9Dv7ODW3TnIMK/R9m/PJb1D4INqYk\n9Bg8To4jKUPUhsDaiHC/sO758cpO1FDf25UQJtJpzC70IVfFoAlKJnyJ+NWZHVSa\n+jNoRtyumzwevYI9nMPHxPWQhbvxaQcb+TCDGO73bAn3tuSPOeL/PaCEAFmyrAnB\n9BhnigiLAgMBAAECggEAGhZ/ZNP1lMNpb9TKcGdSxpr2/kBbPr2DYTeOMQ/ZN20d\nMeSlNe/s3n++5Vy7G9eLwyaMw4UMPdm1vmhEYt08IAtzaZUNbvExVUxzBASiHOE5\nfuDuxv+qLyXVpyco5pG/M8Fi4d+BFe4fuk70+UMixn7omq3GQo2K2G1QnnDZRVLf\npWPVGBFM4n2Nz8Zqoerq9BkhrRedTxk31BwG5CMdLuVYoPFtEVJ0US+z/ZiRHTW8\noO9OwpWYH5UMvp/mnzE3nk2OXMFzXmkCvHH42zEM30qd7liXxR6JMNhSx5dvvIDH\ngT/ThLeVbo1iXnubUseUIJJaKQSeGzF6FekGEUWQ4QKBgQD41Eso7XlyIlHkUJdT\nPkINvNHz7Qh+VZIzUmI6x73k8Lc3gJHttVIzVWLZO2PPjDzIHdCDNbM7Q8aLWXwU\nozx8ACtppqiZPOKPmlqf4ASlA4UoDVgtDyQlDTyiq2VNeJc6hfuknOCBr2IsBmcq\nXfaYD13LvqFUUfg913qCqhA1cQKBgQC9Ap0FKzwtROd6Sm4CUc8nS3uI5xS+kZcx\nDPuUL/y0QUtk2bMPyhcsvvvUqThv6g+hS4yPGdRop6dsgUr0k+H8DMw+639mUGQA\nDx52jXEiq5FkLDYGUtZzcqYUYWlm0TK8115A0IqLNN7T+qo0+moQPVoC6Ql59mUR\n2W0Rv3dvuwKBgQCOMWrd8Y7kHR6lFTTUmhBPH2bvOLYQ07+fWmXN51Tp3nEk43EA\n4QwfT50UPG8WLLKTPQ5p3KcWp+1utm1Esd+fkM0+K8HPB88EdxVwpSiiT7epIq8P\n2MYz8jODVeSm99Gb3PLnp8pX8axtMh+EQ3vme2mDQ+WXdNwiIt/ciWiH0QKBgDgg\n/Iw6aKZw3eTdEYoyzTe4HLltFiX5ilKQrm01SvAkq8UJOTmpI/Svyo0+q3ngoj+d\n9eHRI9joYLnCM4YqHGQDUOwmdzDlEq1J0BwYqM5I8OY0kt6R3wlttJA81VYpLc4S\njvTkeSAZ1UpSGBWff7vO3Foc5imH2D/St6cbmtZdAoGBANX6uVZ9eF6YPfpkKrlA\niTqvPTq8x9S9FXfPDUbDH7iqpStBgly+Mwv7KqRAC9vTzZhKjIRczX8MqlAG4h9X\n6VJdSB2l59r3I+ybZnWfRIFlFatw4pBdyAgFcBzsX2WLxAFaSN5tJk5fVzijFFec\n4XV8ZxY931lL5Brrxf/f1mTg\n-----END PRIVATE KEY-----\n",
+            "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC3t0XFBeg8CCEs\nHsL0ofIGHjYT6pW2D4SU9HCByNg7FAiqTsbrFEcAQ7Va4sWUS80z0yQaDeA5rJDX\nwYFGCNRvTf0vl7xBptN/BHlg+ZNrzfa95DBgMvDiOj9jOdUwmzs6zRTlHejN4suX\nLRWeRpZB+WRBf4auAtgjzTr63BNaJ6Spzoc9Dv7ODW3TnIMK/R9m/PJb1D4INqYk\n9Bg8To4jKUPUhsDaiHC/sO758cpO1FDf25UQJtJpzC70IVfFoAlKJnyJ+NWZHVSa\n+jNoRtyumzwevYI9nMPHxPWQhbvxaQcb+TCDGO73bAn3tuSPOeL/PaCEAFmyrAnB\n9BhnigiLAgMBAAECggEAGhZ/ZNP1lMNpb9TKcGdSxpr2/kBbPr2DYTeOMQ/ZN20d\nMeSlNe/s3n++5Vy7G9eLwyaMw4UMPdm1vmhEYt08IAtzaZUNbvExVUxzBASiHOE5\nfuDuxv+qLyXVpyco5pG/M8Fi4d+BFe4fuk70+UMixn7omq3GQo2K2G1QnnDZRVLf\npWPVGBFM4n2Nz8Zqoerq9BkhrRedTxk31BwG5CMdLuVYoPFtEVJ0US+z/ZiRHTW8\noO9OwpWYH5UMvp/mnzE3nk2OXMFzXmkCvHH42zEM30qd7liXxR6JMNhSx5dvvIDH\ngT/ThLeVbo1iXnubUseUIJJaKQSeGzF6FekGEUWQ4QKBgQD41Eso7XlyIlHkUJdT\nPkINvNHz7Qh+VZIzUmI6x73k8Lc3gJHttVIzVWLZO2PPjDzIHdCDNbM7Q8aLWXwU\nozx8ACtppqiZPOKPmlqf4ASlA4UoDVgtDyQlDTyiq2VNeJc6hfuknOCBr2IsBmcq\nXfaYD13LvqFUUfg913qCqhA1cQKBgQC9Ap0FKzwtROd6Sm4CUc8nS3uI5xS+kZcx\nDPuUL/y0QUtk2bMPyhcsvvvUqThv6g+hS4yPGdRop6dsgUr0k+H8DMw+639mUGQA\nDx52jXEiq5FkLDYGUtZzcqYUYWlm0TK8115A0IqLNN7T+qo0+moQPVoC6Ql59mUR\n2W0Rv3dvuwKBgQCOMWrd8Y7kHR6lFTTUmhBPH2bvOLYQ07+fWmXN51Tp3nEk43EA\n4QwfT50UPG8WLLKTPQ5p3KcWp+1utm1Esd+fkM0+K8HPB88EdxVwpSiiT7epIq8P\n2MYz8jODVeSm99Gb3PLnp8pX8axtMh+EQ3vme2mDQ+WXdNwiIt/ciWiH0QKBgDgg\n/Iw6aKZw3eTdEYoyzTe4HLltFiX5ilKQrm01SvAkq8UJOTmpI/Svyo0+q3ngoj+d\n9eHRI9joYLnCM4YqHGQDUOwmdzDlEq1J0BwYqM5I8OY0kt6R3wlttJA81VYpLc4S\njvTkeSAZ1UpSGBWff7vO3Foc5imH2D/St6cbmtZdAoGBANX6uVZ9eF6YPfpkKrlA\niTqvPTq8x9S9FXfPDUbDH7iqpStBgly+Mwv7KqRAC9vTzZhKjIRczX8MqlAG4h9X\n6VJdSB2l59r3I+ybZnWfRIFlFatw4pBdyAgFcBzsX2WLxAFaSN5tJk5fVzijFFec\n4XV8ZxY931lL5Brrxf/f1mTg\n-----END PRIVATE KEY-----\n",  # noqa
             "client_email": "wallet-web-client@socialpass-358508.iam.gserviceaccount.com",
             "client_id": "114019881192262882667",
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://oauth2.googleapis.com/token",
             "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/wallet-web-client%40socialpass-358508.iam.gserviceaccount.com",
+            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/wallet-web-client%40socialpass-358508.iam.gserviceaccount.com",  # noqa
         }
 
     @staticmethod
@@ -102,7 +102,7 @@ class GoogleTicket:
         event_obj,
         is_insert=True,
         issuer_name="SocialPass",
-        logo_uri="https://res.cloudinary.com/nfty-labs/image/upload/v1660211657/socialpass-ticket-logo_lzcnkm.png",
+        logo_uri="https://res.cloudinary.com/nfty-labs/image/upload/v1660211657/socialpass-ticket-logo_lzcnkm.png",  # noqa
         hex_bg_color="#ef7c4e",
     ):
         """

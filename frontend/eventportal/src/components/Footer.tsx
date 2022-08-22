2077ecc8440b6901d6d842ffaeb3bbd87dc6b3af
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-08-22 11:55:13 -0400

    rename SPTech Group && SP Tech --> SocialPass

--- frontend/eventportal/src/components/Footer.tsx
+++ frontend/eventportal/src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <div className='content d-flex align-items-center mt-auto'>
       <i className='fa-regular fa-question-circle fs-base-p4 text-secondary'></i>
       <span className='ms-auto text-muted'>
-        &copy; 2022, SP Tech Inc.
+        &copy; 2022, SocialPass.
         <span className='d-none d-sm-inline'> All rights reserved</span>
       </span>
     </div>

c250848a6aa1dab01c23af25be0f16e4f8e83728
Tahmid (Halfmoon UI) <67643916+halfmoonui@users.noreply.github.com>
2022-08-25 00:19:00 +0600

    Google Ticket utility integration (#278)
    * [WIP] GoogleTicket.py
    
    * Added ticket generate method, update support for ticket class
    
    * FIxed barcode on ticket
    
    * Remove rendering field for the QR code
    
    * Add latitude and longitude to the ticket class
    
    * Error handling for ticket generation API request
    
    * White-labeling support for ticket class
    
    * fix lint errors
    
    * fix E501 flake8 error
    
    * move google conf to .envs
    
    * updated .envs
    
    Co-authored-by: joaovictorass95 <joaovictorass95@gmail.com>
    Co-authored-by: rizzo <42082827+crypto-rizzo@users.noreply.github.com>

---
+++ backend/apps/root/utilities/GoogleTicket.py
@@ -0,0 +1,182 @@
+import json
+import os
+
+from django.conf import settings
+from google.auth import crypt, jwt
+from google.auth.transport.requests import AuthorizedSession
+from google.oauth2 import service_account
+
+
+class GoogleTicket:
+    """
+    Model for Google Wallet tickets.
+    """
+
+    @staticmethod
+    def get_service_account_info():
+        """
+        Get the service account credentials.
+        """
+        return {
+            "type": "service_account",
+            "project_id": "socialpass-358508",
+            "private_key_id": settings.GOOGLE_WALLET_PRIVATE_KEY_ID,
+            "private_key": settings.GOOGLE_WALLET_PRIVATE_KEY,  # noqa
+            "client_email": "wallet-web-client@socialpass-358508.iam.gserviceaccount.com",
+            "client_id": settings.GOOGLE_WALLET_CLIENT_ID,
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/wallet-web-client%40socialpass-358508.iam.gserviceaccount.com",  # noqa
+        }
+
+    @staticmethod
+    def authenticate(service_account_info, scopes):
+        """
+        Authenticate with the Google API.
+        """
+        credentials = service_account.Credentials.from_service_account_info(
+            service_account_info, scopes=scopes
+        )
+        http_client = AuthorizedSession(credentials)
+
+        return http_client
+
+    @staticmethod
+    def get_issuer_id():
+        """
+        Get the issuer ID.
+        """
+        return os.environ.get("GOOGLE_WALLET_ISSUER_ID")
+
+    @staticmethod
+    def get_ticket_class_payload(event_obj):
+        """
+        Get the payload for inserting/updating a ticket class.
+        """
+        # Create the address from the fields
+        address = ""
+        if event_obj.address_1:
+            address += event_obj.address_1 + "\n"
+        if event_obj.address_2:
+            address += event_obj.address_2 + "\n"
+
+        address_items_list = []
+        if event_obj.city:
+            address_items_list.append(event_obj.city)
+        if event_obj.region:
+            address_items_list.append(event_obj.region)
+        if event_obj.postal_code:
+            address_items_list.append(event_obj.postal_code)
+        if event_obj.country:
+            address_items_list.append(event_obj.country)
+        address += ", ".join(address_items_list)
+
+        # Create the payload
+        payload = {
+            "eventName": {
+                "defaultValue": {"language": "en-us", "value": event_obj.title}
+            },
+            "reviewStatus": "UNDER_REVIEW",
+            "dateTime": {
+                "start": event_obj.start_date.isoformat(),
+                "end": event_obj.end_date.isoformat(),
+            },
+            "venue": {
+                "name": {
+                    "defaultValue": {"language": "en-us", "value": event_obj.location}
+                },
+                "address": {"defaultValue": {"language": "en-us", "value": address}},
+            },
+        }
+
+        # Add the latitude and longitude (if available)
+        if event_obj.lat and event_obj.long:
+            payload["locations"] = [
+                {"latitude": float(event_obj.lat), "longitude": float(event_obj.long)}
+            ]
+
+        return payload
+
+    @staticmethod
+    def insert_update_ticket_class(
+        event_obj,
+        is_insert=True,
+        issuer_name="SocialPass",
+        logo_uri="https://res.cloudinary.com/nfty-labs/image/upload/v1660211657/socialpass-ticket-logo_lzcnkm.png",  # noqa
+        hex_bg_color="#ef7c4e",
+    ):
+        """
+        Insert/update a ticket class. Call post-save when an Event object is
+        created/updated.
+        """
+        service_account_info = GoogleTicket.get_service_account_info()
+        http_client = GoogleTicket.authenticate(
+            service_account_info=service_account_info,
+            scopes=["https://www.googleapis.com/auth/wallet_object.issuer"],
+        )
+        class_id = f"{GoogleTicket.get_issuer_id()}.{str(event_obj.public_id)}"
+        url = "https://walletobjects.googleapis.com/walletobjects/v1/eventTicketClass"
+        payload = GoogleTicket.get_ticket_class_payload(event_obj)
+
+        # Add branding attributes to the payload
+        payload["issuerName"] = issuer_name
+        payload["logo"] = {"sourceUri": {"uri": logo_uri}}
+        payload["hexBackgroundColor"] = hex_bg_color
+
+        # Insert or update the ticket class
+        if is_insert:
+            payload["id"] = class_id
+            response = http_client.post(url, json=payload)
+        else:
+            url = url + "/" + class_id
+            response = http_client.patch(url, json=payload)
+
+        return json.loads(response.text)
+
+    @staticmethod
+    def generate_ticket(ticket_obj):
+        """
+        Generate a Google ticket (pass) create the save to wallet URL and
+        return it along with the token.
+        """
+        # Generate the ticket
+        service_account_info = GoogleTicket.get_service_account_info()
+        http_client = GoogleTicket.authenticate(
+            service_account_info=service_account_info,
+            scopes=["https://www.googleapis.com/auth/wallet_object.issuer"],
+        )
+        ticket_id = "{}.{}".format(
+            GoogleTicket.get_issuer_id(), str(ticket_obj.public_id)
+        )
+        class_id = "{}.{}".format(
+            GoogleTicket.get_issuer_id(),
+            str(ticket_obj.event.public_id),
+        )
+        url = "https://walletobjects.googleapis.com/walletobjects/v1/eventTicketObject"
+        payload = {
+            "id": ticket_id,
+            "classId": class_id,
+            "state": "ACTIVE",
+            "barcode": {"type": "QR_CODE", "value": str(ticket_obj.embed_code)},
+        }
+        response = http_client.post(url, json=payload)
+
+        # Check if there was an error
+        if not (200 <= response.status_code <= 299):
+            return json.loads(response.text)
+
+        # Get the save to wallet URL
+        object_id = json.loads(response.text).get("id")
+        claims = {
+            "iss": http_client.credentials.service_account_email,
+            "aud": "google",
+            "origins": ["socialpass.io"],
+            "typ": "savetowallet",
+            "payload": {"eventTicketObjects": [{"id": object_id}]},
+        }
+        signer = crypt.RSASigner.from_service_account_info(service_account_info)
+        token = jwt.encode(signer, claims).decode("utf-8")
+        save_url = "https://pay.google.com/gp/v/save/%s" % token
+
+        return {"token": token, "save_url": save_url}
--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -337,6 +337,7 @@ CORS_URLS_REGEX = r"^/api/.*$"
 CORS_ALLOW_ALL_ORIGINS = env("CORS_ALLOW_ALL_ORIGINS", default=False)
 
 # Django Invitations - https://github.com/jazzband/django-invitations
+# ------------------------------------------------------------------------------
 INVITATIONS_INVITATION_MODEL = "root.Invite"
 INVITATIONS_ACCOUNT_ADAPTER = "invitations.models.InvitationsAdapter"
 INVITATIONS_INVITATION_ONLY = False
@@ -347,24 +348,36 @@ INVITATIONS_EMAIL_MAX_LENGTH = 254
 INVITATIONS_INVITATION_EXPIRY = 3
 ACCOUNT_ADAPTER = "invitations.models.InvitationsAdapter"
 
-# Django Invitations - https://github.com/jazzband/django-taggit
+# Django Taggit  - https://github.com/jazzband/django-taggit
+# ------------------------------------------------------------------------------
 TAGGIT_CASE_INSENSITIVE = True
 
 # INTERNAL
+# ------------------------------------------------------------------------------
 CHECKOUT_PORTAL_BASE_URL = env("CHECKOUT_PORTAL_BASE_URL")
 SCANNER_BASE_URL = env("SCANNER_BASE_URL")
 
 # STRIPE
+# ------------------------------------------------------------------------------
 STRIPE_PUBLISHABLE_KEY = env("STRIPE_PUBLISHABLE_KEY")
 STRIPE_SECRET_KEY = env("STRIPE_SECRET_KEY")
 
 # MORALIS
+# ------------------------------------------------------------------------------
 MORALIS_API_KEY = env("MORALIS_API_KEY")
 
 # GMAPS
+# ------------------------------------------------------------------------------
 GMAPS_API_KEY = env("GMAPS_API_KEY")
 
+# GWALLET
+# ------------------------------------------------------------------------------
+GOOGLE_WALLET_PRIVATE_KEY_ID = env("GOOGLE_WALLET_PRIVATE_KEY_ID")
+GOOGLE_WALLET_PRIVATE_KEY = env("GOOGLE_WALLET_PRIVATE_KEY")
+GOOGLE_WALLET_CLIENT_ID = env("GOOGLE_WALLET_CLIENT_ID")
+
 # APPLE WALLET
+# ------------------------------------------------------------------------------
 APPLE_WALLET_CERTIFICATE = env("APPLE_WALLET_CERTIFICATE")
 APPLE_WALLET_KEY = env("APPLE_WALLET_KEY")
 APPLE_WALLET_WWDR_CERTIFICATE = env("APPLE_WALLET_WWDR_CERTIFICATE")

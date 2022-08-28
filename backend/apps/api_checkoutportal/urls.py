a057eea23c7edf3fc15a4bb0a00ce24cd2bda081
rizzo <42082827+crypto-rizzo@users.noreply.github.com>
2022-08-28 16:10:28 -0400

    URL name cleanup

--- backend/apps/api_checkoutportal/urls.py
+++ backend/apps/api_checkoutportal/urls.py
@@ -5,17 +5,17 @@ from . import views
 app_name = "api_checkoutportal"
 urlpatterns = [
     path(
-        "retrieve/<uuid:public_id>/",
+        "get/<uuid:public_id>/",
         views.CheckoutPortalRetrieve.as_view(),
         name="retrieve",
     ),
     path(
-        "request/<uuid:public_id>/",
+        "ownership-request/<uuid:public_id>/",
         views.CheckoutPortalOwnershipRequest.as_view(),
         name="request",
     ),
     path(
-        "verify/<uuid:public_id>/",
+        "ownership-verify/<uuid:public_id>/",
         views.CheckoutPortalOwnershipVerify.as_view(),
         name="verify",
     ),
--- backend/apps/router.py
+++ backend/apps/router.py
@@ -28,7 +28,7 @@ urlpatterns = [
 
 # DRF API URLS
 urlpatterns += [
-    path("api/checkout-portal/v1/", include("apps.api_checkoutportal.urls")),
+    path("api/checkout/v1/", include("apps.api_checkoutportal.urls")),
     path("api/scanner/v1/", include("apps.api_scanner.urls")),
 ]
 

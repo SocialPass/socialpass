ae9b52ca3d049b6725f2d08c979a386e6846e991
devjoaov <joaovictorass95@gmail.com>
2022-09-08 08:14:46 -0300

    migrate templatetags directory

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -107,6 +107,7 @@ LOCAL_APPS = [
     "apps.root.apps.RootConfig",
     "apps.dashboard.apps.DashboardConfig",
     "apps.root.apps.OverrideInvitationsConfig",
+    "templatetags",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS

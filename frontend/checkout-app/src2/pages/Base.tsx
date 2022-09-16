f497fdf77e9cad4c894219577bfcaf56e6b59453
Tahmid Khan <tahmid.hm.dev@gmail.com>
2022-09-16 19:01:10 +0600

    More UI fixes

--- frontend/checkout-app/src2/pages/Base.tsx
+++ frontend/checkout-app/src2/pages/Base.tsx
@@ -40,8 +40,11 @@ export default function Base() {
 				{/* Render Child Route End */}
 
 				{/* Footer start */}
-				<div className="content d-flex align-items-center">
-					<i className="fa-regular fa-question-circle fs-base-p4 text-secondary"></i>
+				<div className="content d-flex align-items-center mt-auto">
+					<a href="https://socialpass.gitbook.io/socialpass/" target="_blank" className="text-secondary text-decoration-none fs-base-p4">
+						<i className="fa-regular fa-question-circle"></i>
+						<span className="visually-hidden">Help</span>
+					</a>
 					<span className="ms-auto text-muted">
 						&copy; 2022, SP Tech Inc.<span className="d-none d-sm-inline"> All rights reserved</span>
 					</span>

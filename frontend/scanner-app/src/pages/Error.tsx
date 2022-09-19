f73874e4d32bd1cb2f9795567a1da06ded2ce3fb
mvpedro <mvpedro.cc@gmail.com>
2022-09-19 01:42:49 -0300

    Error page mocking QR reader

--- frontend/scanner-app/src/pages/Error.tsx
+++ frontend/scanner-app/src/pages/Error.tsx
@@ -1,33 +1,64 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { Footer } from '../components/Footer'
 import { FiArrowLeft } from 'react-icons/fi'
-import CloseCircleIcon from '../assets/closeIcon.svg'
+import Html5QrcodePlugin from '../components/Html5QrcodeScannerPlugin/Html5QrcodePlugin'
+import useQRCodeScan from '../hooks/useQRCodeScan'
+import { useEvent } from '../contexts/EventContext'
 import { useNavigate } from 'react-router-dom'
+import { useToast } from '../contexts/ToastContext'
+import { fetchScanTicket } from '../services/api'
+import { ProgressBar } from 'react-bootstrap'
+
 
 export function Error() {
   const navigate = useNavigate()
 
-  function handleGoBack() {
-    navigate(-1)
+  function handleRedirect() {
+    navigate('..')
   }
 
+  useEffect(() => {
+    const scannerContainer = document.getElementById('qr-scanner-container')
+    if (!scannerContainer) {
+      return
+    }
+    ;(scannerContainer.firstChild?.firstChild as any).style.position = ''
+  })
+
+
+
+  const { startQrCode, decodedQRData } = useQRCodeScan({
+    qrcodeMountNodeID: "qrcodemountnode",
+  });
+
+  useEffect(() => {
+    // Add logic to add the camera and scan it
+    startQrCode();
+  }, []);
+
+
   return (
-    <div className='error-footer'>
-      <header className='d-flex flex-row justify-content-between align-items-start py-4 px-4'>
-        <div className='py-2'>
-          <div className='btn-back mt-30'>
-            <FiArrowLeft onClick={handleGoBack} size={26} />
-          </div>
-        </div>
-        <div className='d-flex flex-column align-items-center py-10'>
-          <div className='error-title py-30'>Invalid</div>
-        </div>
-        <div className='mx-40'></div>
-      </header>
-      <div className='error-padding'></div>
-      <div className='error-body d-flex flex-column align-items-center justify-content-center gap-4'>
-        <div>
-          <img src={CloseCircleIcon} />
-        </div>
-        <div className='error-body'>No event was found.</div>
+    <div className='scanner-body d-flex flex-column' >
+      <div className='btn-close' style={{ position: 'fixed', top: 0, left: 0, right: 0, zIndex: 1000 }}>
+        <FiArrowLeft color='#f1f1f1' onClick={handleRedirect} size={26} />
+      </div>
+      <div className='flex-grow-1'>
+
+        {/* <QrReader
+          facingMode={'environment'}
+          delay={500}
+          onError={handleError}
+          onScan={handleScan}
+          style={{ height: '100%', overflow: 'visible', position: 'relative' }}
+        /> */}
+        
+          { <Html5QrcodePlugin 
+          fps={10}
+          qrbox={250}
+          disableFlip={false}
+          //qrCodeSuccessCallback={this.onNewScanResult}
+          /> }
+        
       </div>
     </div>
   )

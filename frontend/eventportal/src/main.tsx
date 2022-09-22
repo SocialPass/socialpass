6a8bd120fdd78ca476475a9c9501b1f8e656a9cd
mvpedro <mvpedro.cc@gmail.com>
2022-09-22 15:41:58 -0300

    Fixing code based on Linter feedbacks

--- frontend/eventportal/src/main.tsx
+++ frontend/eventportal/src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 
 
 Sentry.init({
-  dsn: "https://8227fd5bdda44be2a150e7b6592c5098@o1251100.ingest.sentry.io/6766642",
+  dsn: 'https://8227fd5bdda44be2a150e7b6592c5098@o1251100.ingest.sentry.io/6766642',
   integrations: [new BrowserTracing()],
   tracesSampleRate: 1.0,
 });
--- frontend/scanner-app/src/main.tsx
+++ frontend/scanner-app/src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 
 
 Sentry.init({
-  dsn: "https://27eacbe053014ec7a4d481fd4d489860@o1251100.ingest.sentry.io/6766649",
+  dsn: 'https://27eacbe053014ec7a4d481fd4d489860@o1251100.ingest.sentry.io/6766649',
   integrations: [new BrowserTracing()],
   tracesSampleRate: 1.0,
 });

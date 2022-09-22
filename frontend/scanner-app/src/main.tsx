ff0c6ccdb3eb409af104c90e47129932b5da9861
mvpedro <mvpedro.cc@gmail.com>
2022-09-22 17:00:26 -0300

    Adding enviroment variables regarding Sentry enviroment

--- frontend/scanner-app/src/main.tsx
+++ frontend/scanner-app/src/main.tsx
@@ -9,9 +9,9 @@ Sentry.init({
   dsn: 'https://27eacbe053014ec7a4d481fd4d489860@o1251100.ingest.sentry.io/6766649',
   integrations: [new BrowserTracing()],
   tracesSampleRate: 1.0,
-  sendDefaultPii: false
+  sendDefaultPii: false,
+  environment: import.meta.env.VITE_APP_SENTRY_ENVIRONMENT
 });
-
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(

b904d23c4d0746aed87d9e6b82d1e3b66e37341c
Huberto Kaiser Filho <hubertokf@gmail.com>
2022-09-23 09:25:21 -0300

    fixing Loading page

--- frontend/scanner-app/src/components/EventLoading/EventLoading.tsx
+++ frontend/scanner-app/src/components/EventLoading/EventLoading.tsx
@@ -2,12 +2,9 @@ import { HashLoader } from 'react-spinners'
 
 const EventLoading = () => {
   return (
-    <div className='error-footer'>
-      <div className='error-padding'></div>
-      <div className='d-flex flex-column align-items-center justify-content-center'>
-        <div className='mb-50'>
-          <HashLoader color='#EF7C4E' size={120} />
-        </div>
+    <div className='position-absolute top-auto bottom-50 w-100'>
+      <div className='d-flex flex-column align-items-center justify-content-center gap-30 text-center'>
+        <HashLoader color='#EF7C4E' size={120} />
         <div className='fs-16 fw-500 mt-50'>Loading event, please wait a second...</div>
       </div>
     </div>

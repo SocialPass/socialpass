d08c6823f9106b358a95020d4646081fc6c228ff
devjoaov <joaovictorass95@gmail.com>
2022-09-26 15:15:49 -0300

    refactor scanner services into ticket model

--- backend/apps/api_scanner/tests/test_views.py
+++ backend/apps/api_scanner/tests/test_views.py
@@ -7,7 +7,6 @@ from django.views.generic import TemplateView
 from rest_framework import status
 
 from apps.api_checkoutportal.tests.test_views import prevent_warnings
-from apps.api_scanner.services import redeem_ticket
 from apps.api_scanner.views import SetAccessKeyAndEventMixin
 from apps.root.factories import (
     EventFactory,
@@ -198,7 +197,7 @@ class ScanTicketTestCase(TestCaseWrapper):
         # TEST 409 CONFLICT
         ticket = self.ticket
         redemption_access_key = self.ticket_redemption_key
-        redeemed_ticket = redeem_ticket(ticket, redemption_access_key)
+        redeemed_ticket = ticket.redeem_ticket(redemption_access_key)
         embed_code = (
             str(redeemed_ticket.embed_code) + "/" + str(redeemed_ticket.filename)
         )
---
+++ backend/apps/root/exceptions.py
@@ -0,0 +1,10 @@
+class ForbiddenRedemptionError(Exception):
+    pass
+
+
+class AlreadyRedeemed(Exception):
+    pass
+
+
+class InvalidEmbedCodeError(Exception):
+    pass

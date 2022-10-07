ebcf35e8c72837020311c9f26067d0c8dcf6906b
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2022-10-06 22:33:49 -0400

    delete api_checkout/v0 from apps/router

--- backend/apps/router.py
+++ backend/apps/router.py
@@ -27,7 +27,6 @@ urlpatterns = [
 
 # DRF API URLs
 urlpatterns += [
-    path("api/checkout/v0/", include("apps.api_checkout.v0.urls")),
     path("api/scanner/v1/", include("apps.api_scanner.urls")),
 ]
 

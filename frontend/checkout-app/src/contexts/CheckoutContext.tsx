2556040390f12de269bb62a9eb917c0e22ae1b17
Huberto Kaiser Filho <hubertokf@gmail.com>
2022-10-07 18:06:08 -0300

    checkout methods

--- frontend/checkout-app/src/contexts/CheckoutContext.tsx
+++ frontend/checkout-app/src/contexts/CheckoutContext.tsx
@@ -11,18 +11,44 @@ export const CheckoutContext = createContext<CheckoutContextType>({
 
 export const CheckoutProvider = ({ children }: any) => {
   const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [checkout, setEvent] = useState<Checkout | null>(null)
+  const [checkout, setCheckout] = useState<Checkout | null>(null)
+  const [checkoutItems, setCheckoutItems] = useState([])
+  const [paymentType, setPaymentType] = useState(null)
   const [error, setError] = useState<CheckoutError | null>(null)
 
   const getCheckout = (checkoutPublicId: string) =>
     new Promise((resolve, reject) => {
       setIsLoading(true)
       setError(null)
-      setEvent(null)
+      setCheckout(null)
 
       CheckoutApi.get(checkoutPublicId)
         .then((response) => {
-          setEvent(response.data)
+          setCheckout(response.data)
+          setIsLoading(false)
+          resolve(response.data)
+        })
+        .catch((err) => {
+          setError(err)
+          setIsLoading(false)
+          reject(err)
+        })
+    })
+
+  const saveCheckout = () =>
+    new Promise((resolve, reject) => {
+      setIsLoading(true)
+      setError(null)
+      setCheckout(null)
+
+      const data = {
+        ...checkout,
+        checkoutItems,
+      }
+
+      CheckoutApi.create(data)
+        .then((response) => {
+          setCheckout(response.data)
           setIsLoading(false)
           resolve(response.data)
         })
@@ -37,7 +63,12 @@ export const CheckoutProvider = ({ children }: any) => {
     <CheckoutContext.Provider
       value={{
         checkout,
+        checkoutItems,
+        paymentType,
         getCheckout,
+        setPaymentType,
+        setCheckoutItems,
+        saveCheckout,
         isLoading,
         error,
       }}
--- frontend/checkout-app/src/services/api/CheckoutApi.ts
+++ frontend/checkout-app/src/services/api/CheckoutApi.ts
@@ -12,4 +12,16 @@ export default {
         }),
       )
   },
+
+  create(data: any) {
+    return axios
+      .post('/api/v1/checkout', data)
+      .then((response) => Promise.resolve(response))
+      .catch((error) =>
+        Promise.reject({
+          detail: error.response.data?.detail || 'unknown-error',
+          message: error.response.data?.message || 'unknown-error',
+        }),
+      )
+  },
 }

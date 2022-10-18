3eac19a2768d6e227e2b512e9851b63b7a76240c
mvpedro <mvpedro.cc@gmail.com>
2022-10-18 12:06:26 -0300

    Updating unitary ticket display due to feedbacks

--- frontend/checkout-app/src/components/TicketSelector/index.tsx
+++ frontend/checkout-app/src/components/TicketSelector/index.tsx
@@ -68,7 +68,8 @@ function TicketSelector(props): JSX.Element {
             ) : null}
             {paymentType !== 'tier_asset_ownership' && amount == 0 ? (
               <div className='text-center fs-base-n2 mt-5'>
-                <strong>Price: {getPriceWithCurrencySymbol(ticketTier[paymentType]?.price)} </strong>
+                <strong>Price: &times; 1 </strong>
+                &mdash;{' '}{getPriceWithCurrencySymbol(ticketTier[paymentType]?.price)}
               </div>
             ) : null}
           </div>

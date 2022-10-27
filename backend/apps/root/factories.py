fb84afac4ac4a9b9b29885efc8c155fb845361ea
Tahmid Khan <tahmid.hm.dev@gmail.com>
2022-10-28 00:23:48 +0600

    Add image to Team factory

--- backend/apps/root/factories.py
+++ backend/apps/root/factories.py
@@ -4,6 +4,7 @@ import factory
 import factory.django
 import factory.fuzzy
 from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.utils import timezone
 
 from apps.root.models import (
@@ -39,6 +40,16 @@ class UserFactory(factory.django.DjangoModelFactory):
     password = factory.PostGenerationMethodCall("set_password", "password")
 
 
+def get_mock_team_image():
+    small_gif = (
+        b"\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x00\x00\x00\x21\xf9\x04"
+        b"\x01\x0a\x00\x01\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02"
+        b"\x02\x4c\x01\x00\x3b"
+    )
+    uploaded = SimpleUploadedFile("small.gif", small_gif, content_type="image/gif")
+    return uploaded
+
+
 class TeamFactory(factory.django.DjangoModelFactory):
     """
     Create team with event
@@ -48,6 +59,7 @@ class TeamFactory(factory.django.DjangoModelFactory):
         model = Team
 
     name = factory.Faker("color_name")
+    image = factory.django.ImageField(color="blue")
 
 
 class MembershipFactory(factory.django.DjangoModelFactory):

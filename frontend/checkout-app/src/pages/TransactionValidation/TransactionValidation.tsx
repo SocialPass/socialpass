f84db039fe22925711455ded10151455ab8301ca
mvpedro <mvpedro.cc@gmail.com>
2022-11-01 11:47:56 -0300

    Adding back button functionality on validation page

--- frontend/checkout-app/src/pages/TransactionValidation/TransactionValidation.tsx
+++ frontend/checkout-app/src/pages/TransactionValidation/TransactionValidation.tsx
@@ -31,6 +31,10 @@ export default function TransactionValidation() {
     return timer
   }
 
+  const handleBackClick = () => {
+    navigate(`/${event.public_id}/checkout/${checkout.public_id}`)
+  }
+
   useEffect(() => {
     let interval
 
@@ -45,32 +49,61 @@ export default function TransactionValidation() {
 
   return (
     <>
-      <div className='w-100 hs-200 position-relative'>
-        <div className='d-flex align-items-center justify-content-center w-100 h-100 bg-gray-very-light-lm bg-darkgray-very-dim-dm overflow-hidden pe-none'>
-          <img src={event?.cover_image} className='w-100 h-auto' alt='Cover image'></img>
+      <div className="page-wrapper">
+    <div className="content-wrapper ws-820 mw-100 min-vh-100 mx-auto bg-white-lm bg-dark-very-light-dm d-flex flex-column">
+        <div>
+
+            <div className="w-100 hs-150 position-relative">
+                <div className="d-flex align-items-center justify-content-center w-100 h-100 bg-gray-very-light-lm bg-darkgray-very-dim-dm overflow-hidden pe-none">
+                    <img src={event.cover_image} className="w-100 h-auto" alt="Cover image" />
+                </div>
+
+                <div className="position-absolute z-1 bottom-0 start-0 px-content py-20">
+                    <a href="#" className="btn btn-rounded ps-5 d-flex align-items-center">
+                        <div className="ws-25 hs-25 bg-secondary text-on-secondary rounded-circle d-flex align-items-center justify-content-center">
+                            <i className="fa-regular fa-arrow-left"></i>
+                        </div>
+                        <strong className="text-strong antialiased ms-10"
+                        onClick={() => {
+                          handleBackClick()
+                        }}>Go Back</strong>
+                    </a>
+                </div>
+            </div>
+
+            <div className="px-content pt-20">
+                <p className="text-muted mt-5 mb-0">
+                    By SocialPass
+                </p>
+                <h2 className="text-strong fs-base-p2 fw-700 m-0">
+                    NFT Holders Party
+                </h2>
+            </div>
+
+            <div className="row">
+                <div className="col-md-10 col-lg-8 mx-auto">
+                    <div className="content mt-50 mb-0">
+                        <h1 className="text-strong fw-700 fsr-4 m-0">
+                            Processing Checkout ...
+                        </h1>
+                        <p className="mt-10">
+                            Please wait while we process your order and generate your ticket.
+                        </p>
+                        <div className="progress hs-25 my-50">
+                            <div className="progress-bar progress-bar-secondary progress-bar-animated w-100" role="progressbar"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <div className='position-absolute z-1 top-100 start-50 translate-middle px-content'>
-          <div className='ws-75 hs-75 rounded-circle border border-5 border-blend d-flex align-items-center justify-content-center overflow-hidden bg-gray-very-light-lm bg-darkgray-very-dim-dm'>
-            <img src={event?.team.image} className='d-block w-100 h-auto' alt='Team image'></img>
-          </div>
+        <div className="content text-end border-top pt-10">
+            <span className="ms-auto text-muted fs-base-n2">
+                &copy; 2022, SP Tech Inc. All rights reserved
+            </span>
         </div>
-      </div>
-
-      <div className='px-content pt-20'>
-        <p className='text-muted mt-5 mb-0'>By {event?.team.name}</p>
-        <h2 className='text-strong fs-base-p2 fw-700 m-0'>{event?.title}</h2>
-      </div>
-
-      <div className='row'>
-        <div className='text-center mt-50'>
-          <div className='spinner-border text-primary text-center' role='status' />
-        </div>
-
-        <div className='text-center'>
-          <p className='text-muted mt-5 mb-0'>Please wait, we are validating your transaction...</p>
-        </div>
-      </div>
+    </div>
+</div>
     </>
   )
 }

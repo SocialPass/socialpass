db00a44492b52b0244d76db77be10491eeedf31c
mvpedro <mvpedro.cc@gmail.com>
2022-11-01 09:20:03 -0300

    Adding Error markup to error component

--- frontend/checkout-app/src/pages/Error/Error.tsx
+++ frontend/checkout-app/src/pages/Error/Error.tsx
@@ -2,59 +2,33 @@ import SomethingWentWrong from '../../assets/something_went_wrong.svg'
 
 const Error = () => {
   return (
-    <div className='page-wrapper'>
-      {/* Content wrapper start */}
-      <div className='content-wrapper ws-860 mw-100 min-vh-100 mx-auto bg-white-lm bg-dark-very-light-dm d-flex flex-column'>
-        {/* Main content start */}
-        <div className='content'>
-          <div className='ws-500 mw-100 mx-auto text-center'>
-            <div className='ws-300 mw-100 mx-auto'>
-              <img
-                src={SomethingWentWrong}
-                className='d-block w-100 h-auto'
-                alt='something_went_wrong'
-              />
+    <div className="page-wrapper">
+      <div className="content-wrapper ws-820 mw-100 min-vh-100 mx-auto bg-white-lm bg-dark-very-light-dm d-flex flex-column">
+        <div>
+
+
+          <div className="row">
+            <div className="col-md-10 col-lg-8 mx-auto">
+              <div className="content mt-50 mb-0 text-center">
+                <div className="ws-300 mw-100 mx-auto">
+                  <img src={SomethingWentWrong} className="d-block w-100 h-auto" alt="something_went_wrong" />
+                </div>
+                <h1 className="text-strong fw-700 fsr-4 mb-0">
+                  Something went wrong!!!
+                </h1>
+                <p className="mt-10">
+                  Looks like something went wrong. Please try reloading the page, or try again after some time.
+                </p>
+              </div>
             </div>
-            <h1 className='text-strong fw-700 fsr-1 mb-0'>Something went wrong!</h1>
-            <p>
-              Looks like something went wrong. Please try reloading the page, or try again after
-              some time.
-            </p>
-            <p className='border-top pt-10 mt-10 text-muted fs-base-n4'>
-              If this problem persists, please consider{' '}
-              <a
-                href='https://nftylabs.typeform.com/to/A70KW4vo'
-                className='fw-bold'
-                target='_blank'
-                rel='noreferrer'
-              >
-                contacting us
-              </a>{' '}
-              and letting us know.
-            </p>
           </div>
         </div>
-        {/* Main content end */}
-
-        {/* Footer start */}
-        <div className='content d-flex align-items-center mt-auto'>
-          <a
-            href='https://socialpass.gitbook.io/socialpass/'
-            target='_blank'
-            className='text-secondary text-decoration-none fs-base-p4'
-            rel='noreferrer'
-          >
-            <i className='fa-regular fa-question-circle'></i>
-            <span className='visually-hidden'>Help</span>
-          </a>
-          <span className='ms-auto text-muted'>
-            &copy; 2022, SP Tech Inc.
-            <span className='d-none d-sm-inline'> All rights reserved</span>
-          </span>
-        </div>
-        {/* Footer end */}
+        <div className="content text-end border-top pt-10">
+					<span className="ms-auto text-muted fs-base-n2">
+						&copy; 2022, SP Tech Inc. All rights reserved
+					</span>
+				</div>
       </div>
-      {/* Content wrapper end */}
     </div>
   )
 }

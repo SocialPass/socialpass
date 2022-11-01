de8fb2992e92b26bac849a279ed9266df47ca398
mvpedro <mvpedro.cc@gmail.com>
2022-11-01 11:48:58 -0300

    Fixing typo

--- frontend/checkout-app/src/pages/Error/Error.tsx
+++ frontend/checkout-app/src/pages/Error/Error.tsx
@@ -14,7 +14,7 @@ const Error = () => {
                   <img src={SomethingWentWrong} className="d-block w-100 h-auto" alt="something_went_wrong" />
                 </div>
                 <h1 className="text-strong fw-700 fsr-4 mb-0">
-                  Something went wrong!!!
+                  Something went wrong!
                 </h1>
                 <p className="mt-10">
                   Looks like something went wrong. Please try reloading the page, or try again after some time.

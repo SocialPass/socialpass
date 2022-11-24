0d114e570a3c65246b5d11396c2bce91ab1bb581
mvpedro <mvpedro.cc@gmail.com>
2022-11-24 01:29:38 -0300

    Proposed changes to CountdownTimer styling based on benchmarks

--- frontend/checkout-app/src/components/CountdownTimer/CountdownTimer.tsx
+++ frontend/checkout-app/src/components/CountdownTimer/CountdownTimer.tsx
@@ -26,14 +26,16 @@ function CountdownTimer(props): JSX.Element {
     return (
         <div className='py-20 w-100 h-100'>
             {handleFinishedCountown()}
-            <div className='d-flex align-items-center'>
+            <div className='d-flex align-items-center mt-10 px-10 py-5 rounded bg-gray-very-light-lm bg-darkgray-very-dim-dm border border-dotted fs-base-n2'>
                 <div className='ws-25 flex-shrink-0'>
                     <i className='fa-regular fa-alarm-clock'></i>
                 </div>
-                <div className='fw-bold'>{(isNaN(minutes)) ? '' : zeroLeft(minutes) + ' : '}{(isNaN(seconds)) ? '' : zeroLeft(seconds)}</div>
-            </div>
-            <div className='mt-10 px-10 py-5 rounded bg-gray-very-light-lm bg-darkgray-very-dim-dm border border-dotted fs-base-n2'>
-                After this time, the tickets you have selected will be available for general purchase.
+                <div className='pe-5'>
+                    Checkout time remaining:
+                </div>
+                <div className='fw-bold'>
+                    {(isNaN(minutes)) ? '' : zeroLeft(minutes) + ' : '}{(isNaN(seconds)) ? '' : zeroLeft(seconds)}
+                </div>
             </div>
         </div>
     );

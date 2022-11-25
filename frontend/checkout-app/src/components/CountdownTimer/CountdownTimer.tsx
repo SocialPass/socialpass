25cd3e4de2ff1e626aa30ff726b2de284657a52a
Tahmid Khan <tahmid.hm.dev@gmail.com>
2022-11-25 19:57:28 +0600

    Update design of timer

--- frontend/checkout-app/src/components/CountdownTimer/CountdownTimer.tsx
+++ frontend/checkout-app/src/components/CountdownTimer/CountdownTimer.tsx
@@ -5,46 +5,44 @@ import useCheckout from '@/hooks/useCheckout';
 import propTypes from 'prop-types';
 
 function CountdownTimer(props): JSX.Element {
-    const { expiration } = props
-    const { setCheckout, setCheckoutItems }: any = useCheckout()
-    const { eventPublicId } = useParams()
-    const navigate = useNavigate()
+	const { expiration } = props
+	const { setCheckout, setCheckoutItems }: any = useCheckout()
+	const { eventPublicId } = useParams()
+	const navigate = useNavigate()
 
-    const { minutes, seconds } = useCountdown(expiration)
+	const { minutes, seconds } = useCountdown(expiration)
 
-    function handleFinishedCountown() {
-        if (minutes + seconds <= 0) {
-            setCheckout(null)
-            setCheckoutItems([])
-            navigate(`/${eventPublicId}`)
-            alert("Your checkout session timer has run out. Please try again.")
-        }
-    }
+	function handleFinishedCountown() {
+		if (minutes + seconds <= 0) {
+			setCheckout(null)
+			setCheckoutItems([])
+			navigate(`/${eventPublicId}`)
+			alert("Your checkout session timer has run out. Please try again.")
+		}
+	}
 
-    function zeroLeft(number) {
-        return String(number).padStart(2, '0')
-    }
+	function zeroLeft(number) {
+		return String(number).padStart(2, '0')
+	}
 
-    useEffect(() => {
-        handleFinishedCountown()
-    })
+	useEffect(() => {
+		handleFinishedCountown()
+	})
 
 
-    return (
-        <div className='py-20 w-100 h-100'>
-            <div className='d-flex align-items-center mt-10 px-10 py-5 rounded bg-gray-very-light-lm bg-darkgray-very-dim-dm border border-dotted fs-base-n2'>
-                <div className='ws-25 flex-shrink-0'>
-                    <i className='fa-regular fa-alarm-clock'></i>
-                </div>
-                <div className='pe-5'>
-                    Checkout time remaining:
-                </div>
-                <div className='fw-bold'>
-                    {(isNaN(minutes)) ? '' : zeroLeft(minutes) + ' : '}{(isNaN(seconds)) ? '' : zeroLeft(seconds)}
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className='w-100 h-100 mb-10'>
+			<div className='text-center px-10 py-5 rounded bg-gray-very-light-lm bg-darkgray-very-dim-dm border border-dotted'>
+				<div>
+					<i className='fa-light fa-alarm-clock me-10'></i>
+					Checkout time remaining:
+				</div>
+				<div className='fw-bold'>
+					{(isNaN(minutes)) ? '' : zeroLeft(minutes) + ' : '}{(isNaN(seconds)) ? '' : zeroLeft(seconds)}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default CountdownTimer
@@ -52,6 +50,6 @@ export default CountdownTimer
 
 
 CountdownTimer.propTypes = {
-    counter: propTypes.number,
-    expiration: propTypes.any
+	counter: propTypes.number,
+	expiration: propTypes.any
 };

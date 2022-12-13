4ea9240a7f319d29c1d12fcd0a111353348a5d10
Tahmid Khan <tahmid.hm.dev@gmail.com>
2022-12-13 21:26:54 +0600

    Bug-fix

--- backend/apps/event_discovery/views.py
+++ backend/apps/event_discovery/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import TemplateView, View
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.utils import timezone
 

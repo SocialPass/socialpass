c6e5c7c2c901a133dc8c422c7d4254361128eb73
Tahmid Khan <tahmid.hm.dev@gmail.com>
2022-12-13 20:33:26 +0600

    Handle passcode form on new tickets download page

--- backend/apps/event_discovery/forms.py
+++ backend/apps/event_discovery/forms.py
@@ -5,6 +5,7 @@ class PasscodeForm(forms.Form):
     Form to handle the passcode
     """
     passcode = forms.CharField(
+        min_length=6,
         max_length=6,
         widget=forms.TextInput(
             attrs={"placeholder": "Enter your 6-character passcode"}

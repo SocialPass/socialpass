01f4f5fc95663d76dfdaeda9f7648fe6abb23dab
Tahmid Khan <tahmid.hm.dev@gmail.com>
2023-02-13 14:23:31 +0600

    Make space on headers on event page (for longer timezones)

--- frontend/checkout-app/src/pages/Home/Home.tsx
+++ frontend/checkout-app/src/pages/Home/Home.tsx
@@ -218,12 +218,12 @@ export default function Home() {
                 (checkout?.expiration == null || checkout?.tx_status == 'FULFILLED') ? '' : <CountdownTimer expiration={new Date(checkout?.expiration)} />
               }
             </div>
-            <div className='d-flex align-items-center'>
+            <div className='d-flex align-items-center fs-base-n2'>
               <div className='ws-25 flex-shrink-0'>
                 <i className='fa-regular fa-clock'></i>
               </div>
               <div className='fw-bold'>
-                Date & Time
+                Date
                 {'  '}&mdash;{' '}
                 <i className="fa-light fa-earth-africa me-5"></i>
                 {event?.timezone}
@@ -233,7 +233,7 @@ export default function Home() {
             {event.end_date && (
               <p className='text-muted mt-5 mb-0'><strong className='text-base'>End:</strong> {event?.end_date}</p>
             )}
-            <div className='d-flex align-items-center mt-15'>
+            <div className='d-flex align-items-center fs-base-n2 mt-15'>
               <div className='ws-25 flex-shrink-0'>
                 <i className='fa-regular fa-location-dot'></i>
               </div>

345ebbc679f205e1ff31987ca6933731834d6fc2
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-04-03 22:00:41 -0400

    add code version to rollbar via COMMIT_HASH .env - backend - frontend/scanner - frontend/checkout

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -129,7 +129,7 @@ ROLLBAR_ENV_NAME = env("ROLLBAR_ENV_NAME")
 ROLLBAR = {
     "access_token": "c1f5ed4dcef64a42b1fd2fd2c7718fb4",
     "environment": ROLLBAR_ENV_NAME,
-    "code_version": "1.0",
+    "code_version": env("COMMIT_HASH"),
     "root": ROOT_DIR,
 }
 

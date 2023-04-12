8871e22fb2b8d160f410b86ee04143bd33b07a86
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-04-12 19:08:58 -0400

    remove duplicate settings

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -31,6 +31,12 @@ SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
 SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
     "DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True
 )
+# https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
+X_FRAME_OPTIONS = "DENY"
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-browser-xss-filter
+SECURE_BROWSER_XSS_FILTER = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-httponly
+SESSION_COOKIE_HTTPONLY = True
 
 # Anymail
 # ------------------------------------------------------------------------------

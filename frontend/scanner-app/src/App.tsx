2f344ddb536e5748e178b9490983261e1eb06a92
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-04-12 14:08:06 -0400

    add rollbar npm over script tags

--- frontend/scanner-app/src/App.tsx
+++ frontend/scanner-app/src/App.tsx
@@ -1,18 +1,35 @@
+import { Provider } from '@rollbar/react';
 import { Toaster } from 'react-hot-toast'
 
 import { EventProvider } from './contexts/EventContext'
 import { ThemeProvider } from './contexts/ThemeContext'
-
 import Router from './routes'
+
+const rollbarConfig = {
+   accessToken: "e19adfa974894504b3d7b717c7c5f055",
+   captureUncaught: true,
+   captureUnhandledRejections: true,
+   payload: {
+     client: {
+       javascript: {
+         code_version: import.meta.env.CF_PAGES_COMMIT_SHA,
+         source_map_enabled: true,
+         guess_uncaught_frames: true
+       }
+     }
+   }
+ };
 
 const App = () => {
   return (
+    <Provider config={rollbarConfig}>
     <EventProvider>
       <ThemeProvider>
         <Router />
         <Toaster />
       </ThemeProvider>
     </EventProvider>
+    </Provider>
   )
 }
 

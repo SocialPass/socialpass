dac2fb48496a592193920c333490788ef148fc7f
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-05-01 15:11:48 -0400

    stats for -events_weekly -tickets_weekly -attendees_weekly

--- backend/apps/staff_dashboard/views.py
+++ backend/apps/staff_dashboard/views.py
@@ -3,7 +3,7 @@ from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.db.models.functions import ExtractWeek
 from django.db.models import Count
-from apps.root.models import Event, Team, User
+from apps.root.models import Event, Team, User, Ticket
 
 
 class StatsPageView(TemplateView):
@@ -25,7 +25,7 @@ class StatsPageView(TemplateView):
         current_week = datetime.datetime.now().isocalendar()[1]
         context["weeks"] = [i + 1 for i in range(0, current_week)]
 
-        # Organizer Stats
+        ## Organizer Sign Ups
         organizers_weekly = list(
             User.objects.annotate(week=ExtractWeek("date_joined"))
             .values("week")
@@ -39,6 +39,58 @@ class StatsPageView(TemplateView):
                 if week == i["week"]:
                     context["organizers_weekly"][week] = str(i["total"])
         context["organizers_weekly"] = list(context["organizers_weekly"].values())
+
+        ## Events Created
+        events_weekly = list(
+            Event.objects.annotate(week=ExtractWeek("created"))
+            .values("week")
+            .annotate(total=Count("id"))
+            .values("week", "total")
+        )
+        context["events_weekly"] = {}
+        for week in context["weeks"]:
+            context["events_weekly"][week] = "0"
+            for i in events_weekly:
+                if week == i["week"]:
+                    context["events_weekly"][week] = str(i["total"])
+        context["events_weekly"] = list(context["events_weekly"].values())
+
+        ## Tickets Sold
+        tickets_weekly = list(
+            Ticket.objects.annotate(week=ExtractWeek("created"))
+            .values("week")
+            .annotate(total=Count("id"))
+            .values("week", "total")
+        )
+        context["tickets_weekly"] = {}
+        for week in context["weeks"]:
+            context["tickets_weekly"][week] = "0"
+            for i in tickets_weekly:
+                if week == i["week"]:
+                    context["tickets_weekly"][week] = str(i["total"])
+        context["tickets_weekly"] = list(context["tickets_weekly"].values())
+
+        # Attendees (Tickets Scanend)
+        attendees_weekly = list(
+            Ticket.objects.filter(redeemed=True)
+            .annotate(week=ExtractWeek("created"))
+            .values("week")
+            .annotate(total=Count("id"))
+            .values("week", "total")
+        )
+        context["attendees_weekly"] = {}
+        for week in context["weeks"]:
+            context["attendees_weekly"][week] = "0"
+            for i in attendees_weekly:
+                if week == i["week"]:
+                    context["attendees_weekly"][week] = str(i["total"])
+        context["attendees_weekly"] = list(context["attendees_weekly"].values())
+
+        # Average Tickets (per event)
+
+        # Average Attendees (per event)
+
+        # Average Time to Ticket
         return context
 
 

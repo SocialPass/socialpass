626a855abed9685cc30c3726d20b4ebc2ba24df5
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-05-01 14:56:43 -0400

    stats scaffolded

--- backend/apps/staff_dashboard/views.py
+++ backend/apps/staff_dashboard/views.py
@@ -1,6 +1,9 @@
+import datetime
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
-from apps.root.models import Event, Team
+from django.db.models.functions import ExtractWeek
+from django.db.models import Count
+from apps.root.models import Event, Team, User
 
 
 class StatsPageView(TemplateView):
@@ -14,6 +17,29 @@ class StatsPageView(TemplateView):
     - Average Attendees (per event)
     - Average Time to Ticket
     """
+
+    def get_context_data(self):
+        context = super().get_context_data()
+
+        # Get week number, used in future queries
+        current_week = datetime.datetime.now().isocalendar()[1]
+        context["weeks"] = [i + 1 for i in range(0, current_week)]
+
+        # Organizer Stats
+        organizers_weekly = list(
+            User.objects.annotate(week=ExtractWeek("date_joined"))
+            .values("week")
+            .annotate(total=Count("id"))
+            .values("week", "total")
+        )
+        context["organizers_weekly"] = {}
+        for week in context["weeks"]:
+            context["organizers_weekly"][week] = "0"
+            for i in organizers_weekly:
+                if week == i["week"]:
+                    context["organizers_weekly"][week] = str(i["total"])
+        context["organizers_weekly"] = list(context["organizers_weekly"].values())
+        return context
 
 
 class EventListView(ListView):

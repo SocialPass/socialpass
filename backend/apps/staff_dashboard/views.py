8f92394eec299d0dd2992e280c984e44b1f251c2
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-05-01 09:54:25 -0400

    integrate models into list views

--- backend/apps/staff_dashboard/views.py
+++ backend/apps/staff_dashboard/views.py
@@ -9,9 +9,13 @@ class StatsPageView(TemplateView):
 
 class EventListView(ListView):
     model = Event
+    paginate_by = 25
+    ordering = ["-start_date"]
     template_name = "staff_dashboard/list_events.html"
 
 
 class TeamListView(ListView):
     model = Team
+    paginate_by = 25
+    ordering = ["-modified"]
     template_name = "staff_dashboard/list_teams.html"

d88b91eaf9fed056d67926942311517368257925
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-05-01 10:05:28 -0400

    staff member required middleware

--- backend/apps/staff_dashboard/urls.py
+++ backend/apps/staff_dashboard/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+from django.contrib.admin.views.decorators import staff_member_required
 
 from . import views
 
 app_name = "staff_dashboard"
 
 urlpatterns = [
-    path("", views.StatsPageView.as_view(), name="stats"),
-    path("events/", views.EventListView.as_view(), name="events"),
-    path("teams/", views.TeamListView.as_view(), name="teams"),
+    path("", staff_member_required(views.StatsPageView.as_view()), name="stats"),
+    path("events/", staff_member_required(views.EventListView.as_view()), name="events"),
+    path("teams/", staff_member_required(views.TeamListView.as_view()), name="teams"),
 ]

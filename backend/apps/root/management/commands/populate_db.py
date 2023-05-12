5ac9b6e467ef29d30fb41a186d393d65768b9773
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-05-11 22:25:29 -0400

    functional new script

--- backend/apps/root/management/commands/populate_db.py
+++ backend/apps/root/management/commands/populate_db.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from apps.root.testing import LocalDBScaffold
 
 
-class Command(BaseCommand):
+class Command(BaseCommand, LocalDBScaffold):
     """
     Custom command to pre-populate database with fake data.
     The command also creates a super-user admin@admin.com with
@@ -13,7 +13,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         self.stdout.write(self.style.SUCCESS("Populating..."))
-        LocalDBScaffold.setUp(self)
+        super().setUp()
         self.stdout.write(
             self.style.SUCCESS("Mock data has been populated in the database!")
         )

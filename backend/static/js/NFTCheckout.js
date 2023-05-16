95475b2dafbc58a984bb4dc59d0252dde809eb4e
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-05-16 09:25:54 -0400

    connect wallet / sign message

--- backend/static/js/NFTCheckout.js
+++ backend/static/js/NFTCheckout.js
@@ -3,7 +3,11 @@ import {
   configureChains,
   mainnet,
 } from '@wagmi/core'
+import { getAccount } from '@wagmi/core'
 import { publicProvider } from '@wagmi/core/providers/public'
+import { connect, fetchEnsName } from '@wagmi/core'
+import { signMessage } from '@wagmi/core'
+import { InjectedConnector } from '@wagmi/core/connectors/injected'
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet],
@@ -17,3 +21,25 @@ const config = createConfig({
 })
 
 window.console.log(config);
+
+export async function connectWallet(){
+  // Connect wallet
+  const result = await connect({
+    connector: new InjectedConnector(),
+  })
+  const account = getAccount();
+  console.log(result);
+  console.log(account);
+  document.getElementById("id_wallet_address").value = account.address;
+}
+
+export async function walletSign(message){
+  const signature = await signMessage({
+    message: 'gm wagmi frens',
+  });
+  console.log(signature)
+  document.getElementById("id_signed_message").value = signature;
+}
+
+window.connectWallet = connectWallet;
+window.walletSign = walletSign;

b4cadc1172b32fc3764b5ca783d9214e41903bac
Tahmid Khan <tahmid.hm.dev@gmail.com>
2023-05-16 20:09:51 +0600

    Disconnect wallet method, template changes

--- backend/static/js/NFTCheckout.js
+++ backend/static/js/NFTCheckout.js
@@ -1,45 +1,56 @@
-import {
-  createConfig,
-  configureChains,
-  mainnet,
-} from '@wagmi/core'
-import { getAccount } from '@wagmi/core'
-import { publicProvider } from '@wagmi/core/providers/public'
-import { connect, fetchEnsName } from '@wagmi/core'
-import { signMessage } from '@wagmi/core'
 import { InjectedConnector } from '@wagmi/core/connectors/injected'
+import { publicProvider } from '@wagmi/core/providers/public'
+import {
+	configureChains,
+	connect,
+	createConfig,
+	disconnect,
+	fetchEnsName,
+	getAccount,
+	mainnet,
+	signMessage
+} from '@wagmi/core'
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet],
-  [publicProvider()],
+	[mainnet],
+	[publicProvider()],
 )
 
 const config = createConfig({
-  autoConnect: true,
-  publicClient,
-  webSocketPublicClient,
+	autoConnect: true,
+	publicClient,
+	webSocketPublicClient,
 })
 
 window.console.log(config);
 
-export async function connectWallet(){
-  // Connect wallet
-  const result = await connect({
-    connector: new InjectedConnector(),
-  })
-  const account = getAccount();
-  console.log(result);
-  console.log(account);
-  document.getElementById("id_wallet_address").value = account.address;
+export async function connectWallet() {
+	// Connect wallet
+	const result = await connect({
+		connector: new InjectedConnector(),
+	})
+	const account = getAccount();
+	console.log(result);
+	console.log(account);
+	document.getElementById("id_wallet_address").value = account.address;
 }
 
-export async function walletSign(message){
-  const signature = await signMessage({
-    message: 'gm wagmi frens',
-  });
-  console.log(signature)
-  document.getElementById("id_signed_message").value = signature;
+export async function walletSign(message) {
+	// Sign message
+	const signature = await signMessage({
+		message: 'gm wagmi frens',
+	});
+	console.log(signature)
+	document.getElementById("id_signed_message").value = signature;
+}
+
+export async function disconnectWallet(message) {
+	// Disconnect wallet
+	await disconnect();
+	document.getElementById("id_wallet_address").value = "";
+	document.getElementById("id_signed_message").value = "";
 }
 
 window.connectWallet = connectWallet;
 window.walletSign = walletSign;
+window.disconnectWallet = disconnectWallet;

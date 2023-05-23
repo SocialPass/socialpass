356f18aa481181ba3ad4b976cbbc1a90b7313492
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-05-23 19:33:17 -0400

    swap wallet connection flow with web3modal

--- backend/static/js/NFTCheckout.js
+++ backend/static/js/NFTCheckout.js
@@ -1,121 +1,27 @@
-import { InjectedConnector } from '@wagmi/core/connectors/injected'
-import { CoinbaseWalletConnector } from '@wagmi/core/connectors/coinbaseWallet'
-import { LedgerConnector } from '@wagmi/connectors/ledger'
-import { MetaMaskConnector } from '@wagmi/core/connectors/metaMask'
-import { SafeConnector } from '@wagmi/connectors/safe'
-import { WalletConnectConnector } from '@wagmi/core/connectors/walletConnect'
-import { publicProvider } from '@wagmi/core/providers/public'
-import { watchAccount } from '@wagmi/core'
-import {
-	configureChains,
-	connect,
-	createConfig,
-	disconnect,
-	getAccount,
-	signMessage,
-	mainnet
-} from '@wagmi/core'
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/html'
+import { configureChains, createConfig } from '@wagmi/core'
+import { arbitrum, mainnet, polygon } from '@wagmi/core/chains'
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-	[mainnet],
-	[publicProvider()],
-)
+const chains = [arbitrum, mainnet, polygon]
+const projectId = 'YOUR_PROJECT_ID'
 
-const config = createConfig({
-	autoConnect: false,
-	connectors: [
-		new CoinbaseWalletConnector({
-		  options: {
-			appName: 'socialpass.io',
-			//jsonRpcUrl: 'https://eth-mainnet.alchemyapi.io/v2/yourAlchemyId',
-		  },
-		}),
-		new InjectedConnector(),
-		new LedgerConnector({
-		  chains: chains,
-		}),
-		new MetaMaskConnector(),
-		new SafeConnector({
-		  chains,
-		  options: {
-			allowedDomains: [/gnosis-safe.io$/, /app.safe.global$/],
-			debug: false,
-		  },
-		}),
-		new WalletConnectConnector({
-		  options: {
-			projectId: '...',
-		  },
-		}),
-	],
-	publicClient,
-	webSocketPublicClient,
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  publicClient
 })
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
+const web3modal = new Web3Modal({ projectId }, ethereumClient)
 
-async function connectWallet(connector) {
-	// Connect wallet
-	const result = await connect({
-		connector:connector,
-	})
-	const account = getAccount();
-	console.log(result);
-	console.log(account);
-	//document.getElementById("id_wallet_address").value = account.address;
+export async function openModal(){
+  await web3modal.openModal();
 }
 
-export function setupWallets(){
-	// get container
-	const container = document.getElementById("buttons-container");
-
-	// Iterate over the list and create buttons
-	config.args.connectors.forEach(function(connector) {
-	  console.log(connector)
-	  // Create a button element
-	  const button = document.createElement('button');
-
-	  // Set the button's text content to the current item value
-	  button.textContent = connector.name;
-
-	  // Add an event listener to the button (optional)
-	  button.addEventListener('click', function() {
-		connectWallet(connector)
-	  });
-
-	  // Append the button to the container
-	  container.appendChild(button);
-	});
-
-	// Add Wallet Address Listener
-	watchAccount((account) => {
-		console.log('watching account', account)
-		if (!account.address){
-			disconnectWallet()
-		}
-		// TODO:
-		// Update wallet address HTML value on change
-	});
+export async function closeModal(){
+  await web3modal.openModal();
 }
 
-export async function walletSign(message) {
-	console.log('signing...')
-	// Sign message
-	const signature = await signMessage({
-		message: message,
-	});
-	console.log(signature)
-	//document.getElementById("id_signed_message").value = signature;
-}
-
-export async function disconnectWallet() {
-	// Disconnect wallet
-	console.log('disconnecting...')
-	await disconnect();
-	//document.getElementById("id_wallet_address").value = "";
-	//document.getElementById("id_signed_message").value = "";
-}
-
-
-// Setup globals, etc.
-window.walletSign = walletSign;
-window.disconnectWallet = disconnectWallet;
-setupWallets()
+window.openModal = openModal
+window.closeModal = closeModal

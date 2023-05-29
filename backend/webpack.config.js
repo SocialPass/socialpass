f18424da770e94a99fc3d99e1d2675e42f98820c
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-05-29 10:07:12 -0400

    updated package.json, yarn.lock, bundle, webpack.config

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: ['./static/js/NFTCheckoutModal.js', './static/js/NFTCheckoutWagmi.js'],
+  entry: ['./static/js/NFTCheckoutWagmi.js'],
   output: {
     path: path.resolve(__dirname, 'static/js/dist'),
     filename: 'bundle.js',

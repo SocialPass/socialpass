2c4b68f01e045d10fef817f30a119e54df0fc556
Tahmid Khan <tahmid.hm.dev@gmail.com>
2023-05-30 21:00:05 +0600

    Make name and email read-only, limit NFT stuff only for ASSET_OWNERSHIP flow

--- backend/apps/event_discovery/forms.py
+++ backend/apps/event_discovery/forms.py
@@ -34,8 +34,8 @@ class CheckoutForm2(forms.Form):
     wallet_address = forms.CharField(widget=forms.HiddenInput())
     signed_message = forms.CharField(widget=forms.HiddenInput())
     name = forms.CharField(
-        widget=forms.TextInput(attrs={"placeholder": "Name"}),
+        widget=forms.TextInput(attrs={"placeholder": "Name", "readonly": "readonly"}),
     )
     email = forms.EmailField(
-        widget=forms.EmailInput(attrs={"placeholder": "Email Address"}),
+        widget=forms.EmailInput(attrs={"placeholder": "Email Address", "readonly": "readonly"}),
     )

a1a8a9ef942ba4d18c74d9f30057886ba996a3dc
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-06-20 10:25:02 -0400

    remove unused apps.py

--- backend/apps/dashboard_organizer/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class DashboardOrganizerConfig(AppConfig):
-    name = "apps.dashboard_organizer"
-    verbose_name = _("Dashboard")

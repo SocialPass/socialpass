ba49480f586faf1c2e9049c6f53fa088c644e3d1
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-07-11 21:46:02 -0400

    continued POC: uppy init script

--- backend/static/js/imageUpload.js
+++ backend/static/js/imageUpload.js
@@ -6,47 +6,51 @@ import '@uppy/core/dist/style.min.css';
 import '@uppy/dashboard/dist/style.min.css';
 import '@uppy/image-editor/dist/style.min.css';
 
-// Init Uppy
-const uppy = new Uppy({
-  restrictions: {
-    allowedFileTypes: ['image/*']
-  },
-  debug: true,
-  autoProceed: true,
-  maxNumberOfFiles: 1,
-})
-    .use(Dashboard, {
-      autoOpenFileEditor: true,
-      inline: true,
-      target: '#div_id_cover_image',
-    })
-    .use(ImageEditor, {
-      target: Dashboard,
-      quality: 1,
-      cropperOptions: {
-        initialAspectRatio: 16 / 9,
-        ratio: 16 / 9, // Specify your desired aspect ratio here
-        cropBoxResizable: false,
-        dragMode: 'move',
-        toggleDragModeOnDblclick: false,
-        showGrid: true,
-        showZoomer: true,
-        responsive: true,
-      },
-      actions: {
-        granularRotate: false,
-        cropSquare: false,
-        cropWidescreen: false,
-        cropWidescreenVertical: false,
-      }
-    });
+export function initUppy(target, uploadURL){
+  const uppy = new Uppy({
+    restrictions: {
+      allowedFileTypes: ['image/*']
+    },
+    debug: true,
+    autoProceed: true,
+    maxNumberOfFiles: 1,
+  })
+      .use(Dashboard, {
+        autoOpenFileEditor: true,
+        inline: true,
+        target: target,
+      })
+      .use(ImageEditor, {
+        target: Dashboard,
+        quality: 1,
+        cropperOptions: {
+          initialAspectRatio: 16 / 9,
+          ratio: 16 / 9, // Specify your desired aspect ratio here
+          cropBoxResizable: false,
+          dragMode: 'move',
+          toggleDragModeOnDblclick: false,
+          showGrid: true,
+          showZoomer: true,
+          responsive: true,
+        },
+        actions: {
+          granularRotate: false,
+          cropSquare: false,
+          cropWidescreen: false,
+          cropWidescreenVertical: false,
+        }
+      });
 
-  uppy.on('complete', (result) => {
-    result.successful.forEach((file) => {
-      const reader = new FileReader();
-      reader.onload = function(event) {
-        document.getElementById('id_cover_image').value = event.target.result;
-      };
-      reader.readAsDataURL(file.data);
+    uppy.on('complete', (result) => {
+      result.successful.forEach((file) => {
+        const reader = new FileReader();
+        reader.onload = function(event) {
+          document.getElementById('id_cover_image').value = event.target.result;
+        };
+        reader.readAsDataURL(file.data);
+      });
     });
-  });
+}
+
+// setup windows
+window.initUppy = initUppy

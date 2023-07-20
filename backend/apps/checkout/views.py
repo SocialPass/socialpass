2cb597387bb75f4c3328e38a0df9c1cb082d5d3e
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-07-19 22:31:43 -0400

    bugfix

--- backend/apps/checkout/views.py
+++ backend/apps/checkout/views.py
@@ -54,30 +54,35 @@ class CheckoutPageOne(DetailView):
 
     def get_object(self):
         # Handle default checkout
-        if self.kwargs.get('event_slug'):
-            self.object = Event.objects.select_related("team").prefetch_related(
-                "tickettier_set",
-                "tickettier_set__tier_free",
-                "tickettier_set__tier_asset_ownership",
-            ).get(slug=self.kwargs["event_slug"])
-        # Handle Migrated Checkout (react app)
-        # Page rule from cloudflare tickets.socialpass.io/<UUID> to here
-        if self.kwargs.get('event_uuid_slug'):
-            self.object = Event.objects.select_related("team").prefetch_related(
-                "tickettier_set",
-                "tickettier_set__tier_free",
-                "tickettier_set__tier_asset_ownership",
-            ).get(public_id=self.kwargs["event_uuid_slug"])
-        # Handle Migrated Checkout (redirect to react app)
-        # Limit id to <1000 to only catch early events launched on the react app
-        if self.kwargs.get('event_pk_slug') and self.kwargs['event_pk_slug'] < 1000:
-            self.object = Event.objects.select_related("team").prefetch_related(
-                "tickettier_set",
-                "tickettier_set__tier_free",
-                "tickettier_set__tier_asset_ownership",
-            ).get(pk=self.kwargs["event_pk_slug"])
-        if not self.object:
+        try:
+            if self.kwargs.get('event_slug'):
+                self.object = Event.objects.select_related("team").prefetch_related(
+                    "tickettier_set",
+                    "tickettier_set__tier_free",
+                    "tickettier_set__tier_asset_ownership",
+                ).get(slug=self.kwargs["event_slug"])
+            # Handle Migrated Checkout (react app)
+            # Page rule from cloudflare tickets.socialpass.io/<UUID> to here
+            if self.kwargs.get('event_uuid_slug'):
+                self.object = Event.objects.select_related("team").prefetch_related(
+                    "tickettier_set",
+                    "tickettier_set__tier_free",
+                    "tickettier_set__tier_asset_ownership",
+                ).get(public_id=self.kwargs["event_uuid_slug"])
+            # Handle Migrated Checkout (redirect to react app)
+            # Limit id to <1000 to only catch early events launched on the react app
+            if self.kwargs.get('event_pk_slug') and self.kwargs['event_pk_slug'] < 1000:
+                self.object = Event.objects.select_related("team").prefetch_related(
+                    "tickettier_set",
+                    "tickettier_set__tier_free",
+                    "tickettier_set__tier_asset_ownership",
+                ).get(pk=self.kwargs["event_pk_slug"])
+        except Event.DoesNotExist:
             raise Http404()
+        except:
+            rollbar.report_exc_info()
+
+
         return self.object
 
     def get_context_data(self, *args, **kwargs):

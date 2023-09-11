552822b2066a32e9ceba0274a1fdc37385a3c408
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-09-11 10:28:56 -0400

    fix: add allauth middleware

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -159,6 +159,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 # TEMPLATES

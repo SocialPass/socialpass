f59d0b96a26ddc2e008f3f4a80e6fb1023c20ac8
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-10-14 22:26:46 -0400

    remove

--- backend/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: ['./static/js/NFTCheckoutWagmi.js'],
-  output: {
-    path: path.resolve(__dirname, 'static/js/dist'),
-    filename: 'bundle.js',
- },
-};

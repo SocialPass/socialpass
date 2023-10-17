23da2cf3edf2d2790ccda1f8a7eb79332638221e
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-10-17 00:01:13 -0400

    update rollbar

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -81,7 +81,7 @@ MIDDLEWARE += ["rollbar.contrib.django.middleware.RollbarNotifierMiddleware"]
 ROLLBAR_ENV_NAME = env("ROLLBAR_ENV_NAME")
 
 ROLLBAR = {
-    "access_token": "c1f5ed4dcef64a42b1fd2fd2c7718fb4",
+    "access_token": "411d06b904534d5c82333f20a01d0cd9",
     "environment": ROLLBAR_ENV_NAME,
     "code_version": "1.0",
     "root": str(ROOT_DIR),

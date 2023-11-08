bed5dddc46560c1ee3cc72809bbf38690abd2cd2
Tahmid Khan <tahmid.hm.dev@gmail.com>
2023-11-08 21:59:00 +0600

    Add prefix to checkout URLs

--- backend/apps/checkout/urls.py
+++ backend/apps/checkout/urls.py
@@ -21,40 +21,40 @@ urlpatterns = [
         views.CheckoutPageOneRedirect.as_view(),
         name="checkout_one_redirect",
     ),
-    
+
     # Main checkout flow
     path(
-        "<slug:team_slug>/<slug:event_slug>/",
+        "e/<slug:team_slug>/<slug:event_slug>/",
         views.CheckoutPageOne.as_view(),
         name="checkout_one",
     ),
     re_path(
-        r"(?P<team_slug>[-\w]+)/(?P<event_slug>[-\w]+)/(?P<checkout_type>free|fiat|crypto|nft)/",
+        r"e/(?P<team_slug>[-\w]+)/(?P<event_slug>[-\w]+)/(?P<checkout_type>free|fiat|crypto|nft)/",
         views.CheckoutPageOne.as_view(),
         name="checkout_one",
     ),
     path(
-        "<slug:team_slug>/<slug:event_slug>/checkout/<uuid:checkout_session_public_id>/",
+        "e/<slug:team_slug>/<slug:event_slug>/checkout/<uuid:checkout_session_public_id>/",
         views.CheckoutPageTwo.as_view(),
         name="checkout_two",
     ),
     path(
-        "<slug:team_slug>/<slug:event_slug>/checkout-fiat/<uuid:checkout_session_public_id>/",
+        "e/<slug:team_slug>/<slug:event_slug>/checkout-fiat/<uuid:checkout_session_public_id>/",
         views.CheckoutFiat.as_view(),
         name="checkout_fiat",
     ),
     path(
-        "<slug:team_slug>/<slug:event_slug>/stripe-cancel/<uuid:checkout_session_public_id>/<str:token>/",
+        "e/<slug:team_slug>/<slug:event_slug>/stripe-cancel/<uuid:checkout_session_public_id>/<str:token>/",
         views.StripeCheckoutCancel.as_view(),
         name="stripe_checkout_cancel",
     ),
     path(
-        "<slug:team_slug>/<slug:event_slug>/stripe-success/<uuid:checkout_session_public_id>/<str:token>/",
+        "e/<slug:team_slug>/<slug:event_slug>/stripe-success/<uuid:checkout_session_public_id>/<str:token>/",
         views.StripeCheckoutSuccess.as_view(),
         name="stripe_checkout_success",
     ),
     path(
-        "<slug:team_slug>/<slug:event_slug>/success/<uuid:checkout_session_public_id>/",
+        "e/<slug:team_slug>/<slug:event_slug>/success/<uuid:checkout_session_public_id>/",
         views.CheckoutPageSuccess.as_view(),
         name="checkout_success",
     ),

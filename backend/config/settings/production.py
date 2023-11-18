fda0ed8b90ee450bd0379b9836976c2e0111c853
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-11-18 14:45:03 -0500

    fix / bump rollbar

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -129,7 +129,7 @@ MIDDLEWARE += ["rollbar.contrib.django.middleware.RollbarNotifierMiddleware"]
 ROLLBAR_ENV_NAME = env("ROLLBAR_ENV_NAME")
 
 ROLLBAR = {
-    "access_token": "c1f5ed4dcef64a42b1fd2fd2c7718fb4",
+    "access_token": "411d06b904534d5c82333f20a01d0cd9",
     "environment": ROLLBAR_ENV_NAME,
     "code_version": env("COMMIT_HASH"),
     "root": str(ROOT_DIR),

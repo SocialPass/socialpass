295015e4867866f06cd4b0b5655e588c20b96a2e
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-12-07 13:25:20 -0500

    max from 20 -> 10

--- backend/apps/root/admin.py
+++ backend/apps/root/admin.py
@@ -34,8 +34,7 @@ class CustomDBAdmin(admin.ModelAdmin):
 
     list_display = ["public_id", "created", "modified"]
     sortable_by = ["public_id", "created", "modified"]
-    list_per_page = 20
-
+    list_per_page = 10
 
 @admin.register(CheckoutItem)
 class CheckoutItemAdmin(CustomDBAdmin):

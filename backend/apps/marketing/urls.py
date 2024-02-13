a117cfba3bfb068fcd759496a8010c04e83f9b47
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2024-02-13 08:35:05 -0500

    sitemap fix

--- backend/apps/marketing/urls.py
+++ backend/apps/marketing/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "marketing"
 
 urlpatterns = [
-    path("", views.MarketingIndex.as_view(), name="index"),
-    path("host/", views.MarketingHostIndex.as_view(), name="index_host"),
+    path("", views.MarketingIndex.as_view(), name="index")
+    # path("host/", views.MarketingHostIndex.as_view(), name="index_host"),
     # path("events/browse", views.EventDiscoveryBrowse.as_view(), name="browse"),
 ]

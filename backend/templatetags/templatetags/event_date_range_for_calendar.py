87441b23e725986d23f35140fde45896b3da1d81
Tahmid Khan <tahmid.hm.dev@gmail.com>
2024-02-20 16:34:17 +0600

    Add to calendar dates fail silently in case of error

--- backend/templatetags/templatetags/event_date_range_for_calendar.py
+++ backend/templatetags/templatetags/event_date_range_for_calendar.py
@@ -7,11 +7,10 @@ register = template.Library()
 
 @register.simple_tag(takes_context=False)
 def event_date_range_for_calendar(event):
-	result = False
+	result = ""
 
 	try:
 		if event.start_date and event.timezone:
-			result = ""
 			tz_offset = event.start_date.astimezone(ZoneInfo(event.timezone)).strftime("%z")
 			result += event.start_date.strftime("%Y/%m/%d %H:%M:%S") + tz_offset + "|"
 
@@ -22,6 +21,6 @@ def event_date_range_for_calendar(event):
 				end_date = event.start_date + timedelta(hours=1)
 				result += end_date.strftime("%Y/%m/%d %H:%M:%S") + tz_offset
 	except Exception:
-		result = False
+		result = ""
 
 	return result

21e38a665b772cf8b8b52c150beecea343493ea3
Tahmid Khan <tahmid.hm.dev@gmail.com>
2024-03-13 20:43:42 +0600

    New event stats template

--- backend/apps/dashboard_organizer/views.py
+++ backend/apps/dashboard_organizer/views.py
@@ -614,15 +614,6 @@ class EventStatsView(TeamContextMixin, DetailView):
         # Tickets redeemed
         context["tickets_scanned_count"] = event.tickets_scanned_count
 
-        # VIP list
-        context["manual_attendees"] = ManualAttendee.objects.filter(
-            event=context["event"]
-        ).order_by("-created")
-        context["manual_attendees_redeemed_count"] = 0
-        for manual_attendee in context["manual_attendees"]:
-            if manual_attendee.redeemed_at:
-                context["manual_attendees_redeemed_count"] += 1
-
         return context
 
 

6dea0b1de973240c751ad2600e97c23a0a1591f1
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2024-04-25 21:40:49 -0400

    remove context processor print

--- apps/root/context_processors.py
+++ apps/root/context_processors.py
@@ -2,7 +2,5 @@ import os
 
 def export_vars(request):
 	data = {}
-	print("HELLO")
-	print(os.environ['GOOGLE_MAPS_API_KEY'])
 	data['GOOGLE_MAPS_API_KEY'] = os.environ['GOOGLE_MAPS_API_KEY']
 	return data

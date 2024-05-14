45bc8b09db28f189d0ef5fd64d1e3fb0e43b30a9
0xRizzo <42082827+crypto-rizzo@users.noreply.github.com>
2024-05-13 20:52:16 -0400

    Revert "Comment"
    This reverts commit d01d80307f7fc4397daca1fdf3cfa82044090fa5.

--- apps/root/models.py
+++ apps/root/models.py
@@ -216,7 +216,7 @@ class Team(DBModel):
 
     def get_whitelabel(self):
         """
-        Get team whitelabel, or the global one. Used for Google and Apple tickets.
+        Get team whitelabel, or the global one. Used for Google tickets.
         """
         whitelabel = False
 

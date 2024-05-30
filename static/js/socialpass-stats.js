f77e77fddd7bfdd1b2c8e4dcbfc41a852b93f93f
halfmoonui <hey.halfmoon@gmail.com>
2024-05-30 15:38:13 +0600

    Small improvement:

--- static/js/socialpass-stats.js
+++ static/js/socialpass-stats.js
@@ -10,7 +10,9 @@ try {
   diff = (now.getTime() - statsSent.getTime()) / 1000;
   diff /= (60 * 60);
   diff = Math.abs(Math.round(diff));
-} catch {}
+} catch {
+  diff = 24;
+}
 
 // Send if 24 hours have passed (so once a day)
 if (diff >= 24) {

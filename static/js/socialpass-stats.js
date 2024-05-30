6d89f879cd31a7f3fdd9eff924722be1b9578092
halfmoonui <hey.halfmoon@gmail.com>
2024-05-30 15:23:56 +0600

    Added socialpass-analytics.js

---
+++ static/js/socialpass-stats.js
@@ -0,0 +1,19 @@
+("use strict");
+
+const url = "https://analytics.socialpass.io";
+
+// Get the diff in hours
+let diff = 0;
+try {
+  const now = new Date();
+  const statsSent = new Date(localStorage.getItem("socialpass:stats-sent"));
+  diff = (now.getTime() - statsSent.getTime()) / 1000;
+  diff /= (60 * 60);
+  diff = Math.abs(Math.round(diff));
+} catch {}
+
+// Send if 24 hours have passed (so once a day)
+if (diff >= 24) {
+  fetch(url, { method: "GET" });
+  localStorage.setItem("socialpass:stats-sent", String(new Date()));
+}

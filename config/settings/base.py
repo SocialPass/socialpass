52914ef30bb8364479af8a3f9a749bb7d87e7bea
0xRizzo <42082827+crypto-rizzo@users.noreply.github.com>
2024-06-05 10:45:03 -0400

    minor/sp-license-registry (#1245)
    * closes #1208
    
    * linting/formatting
    
    * fix: remove unrelated changes in config/settings/base
    
    * linting/formatting

--- config/settings/base.py
+++ config/settings/base.py
@@ -171,6 +171,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
+    "config.settings.middleware.LicenseMiddleware",
 ]
 
 # TEMPLATES

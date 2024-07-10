64ef7de0fd168a51a2bfbd43d6c7dfe947a58fb8
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2024-07-10 15:14:19 -0400

    default string for license key

--- config/settings/base.py
+++ config/settings/base.py
@@ -378,4 +378,4 @@ SOCIALPASS_INTEGRATIONS = integrations.SOCIALPASS_INTEGRATIONS
 
 # LICENSE KEY
 # ------------------------------------------------------------------------------
-LICENSE_KEY = env("LICENSE_KEY")
+LICENSE_KEY = env("LICENSE_KEY", default="")
